c20ef8489dd7a3c1988e55d2cc7aaf2045fe9bbf
JinsuYeo <68772263+JinsuYeo@users.noreply.github.com>
2021-11-10 21:31:49 +0900

    Initial commit

---
+++ c++/c++/main.cpp
@@ -0,0 +1,74 @@
+//
+//  main.cpp
+//  c++
+//
+//  Created by 여진수 on 2021/11/06.
+//
+
+#include <iostream>
+#include <climits>
+#include <vector>
+#include <iomanip>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    char choice {};
+    vector<int> numbers {};
+    
+    cout << "P - print numbers\nA - Add a number\n";
+    cout << "M - Display mean of the numers\nS - Display the smallest number\n";
+    cout << "L - Display largest number\nQ - Quit\n\n";
+    
+    int int_add {};
+    
+    do {
+        cout << "Enter your choice: ";
+        cin >> choice;
+        switch(choice) {
+            case 'p':
+            case 'P': if(numbers.size() != 0) {
+                cout << "[ ";
+                for(auto number: numbers) {
+                    cout << number << " ";
+                }
+                cout << "]" << endl;
+            } else {
+                cout << "[] - the list is empty";
+            }
+                break;
+            case 'a':
+            case 'A': cout << "Enter an integer to add to the list: ";
+                cin >> int_add;
+                numbers.push_back(int_add);
+                cout << int_add << " added\n";
+                break;
+            case 'm':
+            case 'M': {
+                int avr{}, sum {};
+                for(auto number: numbers) {
+                    sum += number;
+                }
+                avr = sum/numbers.size();
+                cout << "The mean is : " << avr << "\n";
+                break;
+            }
+            case 's':
+            case 'S':
+            
+            case 'l':
+            case 'L':
+                
+            case 'q':
+            case 'Q':
+                
+            default: cout << "Unknown selection, please try again\n";
+                break;
+        }
+        printf("\n\n");
+    } while(choice != 'q' || choice != 'Q');
+    
+    
+
+    return 0;
+}

9ed614347929fc2e4587b89c6ef21ac5adb626b4
JinsuYeo <68772263+JinsuYeo@users.noreply.github.com>
2021-11-18 22:42:40 +0900

    finding day of input date

--- cpp algorithm/cpp algorithm/main.cpp
+++ cpp algorithm/cpp algorithm/main.cpp
@@ -1,13 +1,27 @@
 #include <iostream>
+#include <vector>
+
+enum day {SUN, MON, TUE, WED, THU, FRI, SAT};
 
 using namespace std;
 
 int main(int argc, const char * argv[]) {
-    int a, b;
-    while(cin >> a >> b) {
-        cout << a + b << '\n';
+    enum day days;
+    char* day[] = {"SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"};
+    int mon {}, date {};
+    cin >> mon >> date;
+    int sum{};
+    int d{};
+    vector<int> mondate {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    for(int i{}; i < mon; i++){
+        sum += mondate[i];
     }
+    d = (sum + date)%7;
     
+    cout << day[d] << '\n';
+    
+        
     return 0;
 }
 
+ 

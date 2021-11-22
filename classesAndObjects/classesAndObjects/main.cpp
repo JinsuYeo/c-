d2f807fa8aea6e1acc2b1b084357876a3ba7d16c
JinsuYeo <68772263+JinsuYeo@users.noreply.github.com>
2021-11-22 22:49:54 +0900

    deep copy
    deep copy study

--- classesAndObjects/classesAndObjects/main.cpp
+++ classesAndObjects/classesAndObjects/main.cpp
@@ -1,19 +1,45 @@
 #include <iostream>
 #include <string>
 
-class Player {
-    std::string name;
-    int health;
-    int xp;
-    
-    void talk(std::string);
-    bool is_dead();
+class Deep {
+private:
+    int *data;
+public:
+    void set_data_value(int d){*data = d;}
+    int get_data_value(){return *data;}
+    Deep(int d);
+    Deep(const Deep &source);
+    ~Deep();
 };
 
-int main(int argc, const char * argv[]) {
-    Player* frank = new Player;
-    Player hero;
+Deep::Deep(int d) {
+    data = new int;
+    *data = d;
+}
 
+Deep::~Deep(){
+    delete data;
+    std::cout << "Destructor freeing data" << '\n';
+}
+
+Deep::Deep(const Deep &source)
+    : Deep(*source.data) {
+    std::cout << "Copy constructor - deep\n";
+}
+
+void display_deep(Deep s){
+    std::cout << s.get_data_value() << std::endl;
+}
+
+
+int main(int argc, const char * argv[]) {
+    Deep obj1 {100};
+    display_deep(obj1);
+    
+    Deep obj2 {obj1};
+    
+    obj2.set_data_value(1000);
+    display_deep(obj2);
 
     return 0;
 }

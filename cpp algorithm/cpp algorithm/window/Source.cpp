de39f2e4bf0bf547fee9064f3484396c83511fa3
JinsuYeo <68772263+JinsuYeo@users.noreply.github.com>
2021-12-30 12:42:58 +0900

    동적 프로그래밍 연습
    백준 9095

--- cpp algorithm/cpp algorithm/window/Source.cpp
+++ cpp algorithm/cpp algorithm/window/Source.cpp
@@ -3,20 +3,29 @@
 
 using namespace std;
 
-int arr[1001];
+int arr[11];
 
-int dp(int a) {
-	if (a == 1) return 1;
-	if (a == 2) return 2;
-	if (arr[a] != 0) return arr[a];
-	return arr[a] = (dp(a - 1) + dp(a - 2))%10007;
+int dp(int n) {
+	if (n == 1) return 1;
+	if (n == 2) return 2;
+	if (n == 3) return 4;
+	if (arr[n] != 0) return arr[n];
+	return arr[n] = dp(n - 1) + dp(n - 2) + dp(n - 3);
 }
 
 int main() {
 	int a{};
-
 	cin >> a;
-	cout << dp(a);
+
+	for (int i = 0; i < a; i++) {
+		for (int j = 0; j < 11; j++) {
+			arr[j] = 0;
+		}
+		int n{};
+
+		cin >> n;
+		cout << dp(n) << "\n";
+	}
 
 	return 0;
 }

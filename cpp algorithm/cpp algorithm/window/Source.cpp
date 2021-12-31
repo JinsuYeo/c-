13e3b363434de07139f86aab64061178d49b1860
JinsuYeo <68772263+JinsuYeo@users.noreply.github.com>
2021-12-31 17:32:58 +0900

    동적 프로그래밍 연습
    백준 11057
    
    arr[i][j] = arr[i-1][j] + arr[i-j][j+1] + ...
     + arr[i-1][1];
    나머지 연산자 19번 라인에 안해서 한번 틀림. 까먹지 않도록 하기.

--- cpp algorithm/cpp algorithm/window/Source.cpp
+++ cpp algorithm/cpp algorithm/window/Source.cpp
@@ -3,24 +3,30 @@
 using namespace std;
 
 int main(int argc, const char* argv[]) {
-    int n[3][4]{}, count[3]{};
-    char result[5] = { 'D', 'C', 'B', 'A', 'E' };
+    int n{}, result{};
+    cin >> n;
+    int arr[1001][10]{};
 
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 4; j++)
-            cin >> n[i][j];
+    for (int i = 0; i < 10; i++) {
+        arr[1][i] = 1;
     }
 
+    for (int i = 2; i <= n; i++) {
 
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 4; j++)
-            if (n[i][j]) {
-                count[i]++;
+        for (int j = 0; j < 10; j++) {
+            for (int k = j; k < 10; k++) {
+                arr[i][j] += arr[i - 1][k] % 10007;
+                arr[i][j] %= 10007;
             }
-        cout << result[count[i]] << endl;
+        }
     }
 
+    for (int i = 0; i < 10; i++) {
+        result += arr[n][i] % 10007;
+        result %= 10007;
+    }
 
+    cout << result;
 
     return 0;
 }

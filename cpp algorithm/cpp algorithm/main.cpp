818a656dc1171ae9a25af9448dd813221588c65e
JinsuYeo <68772263+JinsuYeo@users.noreply.github.com>
2022-01-16 16:31:45 +0900

    동적 프로그래밍 연습 백준 2156
    포도주를 3개 연속 선택 불가능 + 최대한 많은 양.
    
    1의 최댓값은 1, 2의 최댓값은 1+2, 3의 최댓값은 1+2 or 1+3 or 2+3...
    
    따라서 n의 최댓값은 n-1까지의 최댓값 or n-2까지의 최댓값 + n번쩨 값 or n-3까지의 최댓값 + n-1번째 값 + n번째 값.

--- cpp algorithm/cpp algorithm/main.cpp
+++ cpp algorithm/cpp algorithm/main.cpp
@@ -3,35 +3,24 @@
 
 using namespace std;
 
-int dp[2][100001], arr[2][100001];
+int dp[10001], arr[10001];
 
 int main(int argc, const char * argv[]) {
-    int n{}, cn{};
-    
+    int n{};
     cin >> n;
-    for(int i {}; i < n; i++) {
-        cin >> cn;
-        for (int j{}; j < 2; j++) {
-            for(int k{}; k < cn; k++) {
-                cin >> arr[j][k];
-            }
-        }
-        dp[0][0] = arr[0][0];
-        dp[1][0] = arr[1][0];
-        dp[0][1] = arr[1][0] + arr[0][1];
-        dp[1][1] = arr[0][0] + arr[1][1];
-        
-        for (int j{2}; j < cn; j++) {
-            for(int k{}; k < 2; k++) {
-                if(!k) {
-                    dp[0][j] = arr[0][j] + max(dp[1][j-1], dp[1][j-2]);
-                } else {
-                    dp[1][j] = arr[1][j] + max(dp[0][j-1], dp[0][j-2]);
-                }
-            }
-        }
-        cout << max(dp[0][cn-1],dp[1][cn-1]) << endl;
+    for (int i {1}; i <= n; i++) {
+        cin >> arr[i];
     }
+    
+    dp[0] = 0;
+    dp[1] = arr[1];
+    dp[2] = arr[1] + arr[2];
+    
+    for(int i {3}; i <= n; i++) {
+        dp[i] = max(dp[i-1], max(dp[i-2] + arr[i], dp[i-3] + arr[i-1] + arr[i]));
+    }
+    
+    cout << dp[n] << endl;
     
     return 0;
 }

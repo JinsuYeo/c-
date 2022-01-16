e67c330d1d74de2354a03d5595ffbf88c45c6590
JinsuYeo <68772263+JinsuYeo@users.noreply.github.com>
2022-01-16 12:03:10 +0900

    동적프로그래밍 스터디 백준 9465
    1행일 경우 dp[0][j] = arr[0][j] + max(dp[1][j-1], dp[1][j-2]);
    
    출력 신경쓰기(endl 붙이기)

--- cpp algorithm/cpp algorithm/main.cpp
+++ cpp algorithm/cpp algorithm/main.cpp
@@ -3,20 +3,35 @@
 
 using namespace std;
 
+int dp[2][100001], arr[2][100001];
+
 int main(int argc, const char * argv[]) {
-    int h{}, m{};
-    cin >> h >> m;
+    int n{}, cn{};
     
-    if(m < 45) {
-        m = 15 + m;
-        if(h == 0)
-            h = 23;
-        else h--;
-    } else {
-        m -= 45;
+    cin >> n;
+    for(int i {}; i < n; i++) {
+        cin >> cn;
+        for (int j{}; j < 2; j++) {
+            for(int k{}; k < cn; k++) {
+                cin >> arr[j][k];
+            }
+        }
+        dp[0][0] = arr[0][0];
+        dp[1][0] = arr[1][0];
+        dp[0][1] = arr[1][0] + arr[0][1];
+        dp[1][1] = arr[0][0] + arr[1][1];
+        
+        for (int j{2}; j < cn; j++) {
+            for(int k{}; k < 2; k++) {
+                if(!k) {
+                    dp[0][j] = arr[0][j] + max(dp[1][j-1], dp[1][j-2]);
+                } else {
+                    dp[1][j] = arr[1][j] + max(dp[0][j-1], dp[0][j-2]);
+                }
+            }
+        }
+        cout << max(dp[0][cn-1],dp[1][cn-1]) << endl;
     }
-    
-    cout << h << " " << m << endl;
     
     return 0;
 }

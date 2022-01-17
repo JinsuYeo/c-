74356cd437fda2318bdda53ba7ae8a15dbe0f053
JinsuYeo <68772263+JinsuYeo@users.noreply.github.com>
2022-01-17 14:16:07 +0900

    동적 프로그래밍 연습 백준 11055
    11053번과 비슷해서 풀기 쉬웠음.
    dp[i]에 arr[i]보다 작고 dp[i]보다 dp[j]+arr[i]가 큰 수가 있을 떄(arr[j]), dp[i] = dp[j] + arr[i];
    마찬가지로 초기값 설정 잘할 것(max = dp[1]).

--- cpp algorithm/cpp algorithm/main.cpp
+++ cpp algorithm/cpp algorithm/main.cpp
@@ -6,18 +6,19 @@ using namespace std;
 int dp[1001], arr[1001];
 
 int main(int argc, const char * argv[]) {
-    int n{}, max{1};
+    int n{}, max{};
     cin >> n;
     for (int i {1}; i <= n; i++) {
         cin >> arr[i];
     }
     
-    dp[1] = 1;
+    dp[1] = arr[1];
+    max = dp[1];
     for(int i {2}; i <= n; i++) {
-        dp[i] = 1;
+        dp[i] = arr[i];
         for(int j{1}; j <= i-1; j++) {
-            if(arr[j] < arr[i] && dp[i] < dp[j] + 1) {
-                dp[i] = dp[j] + 1;
+            if(arr[j] < arr[i] && dp[i] < dp[j] + arr[i]) {
+                dp[i] = dp[j] + arr[i];
             }
         }
         if(max < dp[i]) max = dp[i];

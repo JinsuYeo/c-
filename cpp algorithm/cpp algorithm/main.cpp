36c888368dbe19b88b180069b3476abf6a78897a
JinsuYeo <68772263+JinsuYeo@users.noreply.github.com>
2022-01-17 14:07:55 +0900

    동적 프로그래밍 연습 백준 11053
    입력된 수열의 위치별 숫자의 가능한 긴 수열의 길이를 dp 배열에 저장해 놓은 뒤 동적 프로그래밍으로 처리(이미 저장된 가장 긴 길이(dp[j]) 위치의 숫자(arr[j])보다 큰 수가 뒤에 존재(arr[i])할 때 dp[i]+1).
    초기조건 생각하기(max를 1로 초기화해놔야 1개의 숫자만 입력되었을 떄 1이 나옴).

--- cpp algorithm/cpp algorithm/main.cpp
+++ cpp algorithm/cpp algorithm/main.cpp
@@ -3,24 +3,27 @@
 
 using namespace std;
 
-int dp[10001], arr[10001];
+int dp[1001], arr[1001];
 
 int main(int argc, const char * argv[]) {
-    int n{};
+    int n{}, max{1};
     cin >> n;
     for (int i {1}; i <= n; i++) {
         cin >> arr[i];
     }
     
-    dp[0] = 0;
-    dp[1] = arr[1];
-    dp[2] = arr[1] + arr[2];
-    
-    for(int i {3}; i <= n; i++) {
-        dp[i] = max(dp[i-1], max(dp[i-2] + arr[i], dp[i-3] + arr[i-1] + arr[i]));
+    dp[1] = 1;
+    for(int i {2}; i <= n; i++) {
+        dp[i] = 1;
+        for(int j{1}; j <= i-1; j++) {
+            if(arr[j] < arr[i] && dp[i] < dp[j] + 1) {
+                dp[i] = dp[j] + 1;
+            }
+        }
+        if(max < dp[i]) max = dp[i];
     }
     
-    cout << dp[n] << endl;
+    cout << max << endl;
     
     return 0;
 }

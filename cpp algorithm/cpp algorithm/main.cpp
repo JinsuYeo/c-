b829e32be6a05461690f6e62ce6efd32ea872dec
JinsuYeo <68772263+JinsuYeo@users.noreply.github.com>
2022-01-17 14:22:50 +0900

    동적 프로그래밍 연습 백준 11722
    마찬가지로 백준 11053번과 비슷해서 쉬웠음.
    11053번과 반대로 arr[i]보다 큰 arr[j]가 앞에 존재할 때, dp[i] = dp[j] + 1;
    이번에는 초기값 설정 잘했음(max = 1).

--- cpp algorithm/cpp algorithm/main.cpp
+++ cpp algorithm/cpp algorithm/main.cpp
@@ -12,13 +12,13 @@ int main(int argc, const char * argv[]) {
         cin >> arr[i];
     }
     
-    dp[1] = arr[1];
+    dp[1] = 1;
     max = dp[1];
     for(int i {2}; i <= n; i++) {
-        dp[i] = arr[i];
+        dp[i] = 1;
         for(int j{1}; j <= i-1; j++) {
-            if(arr[j] < arr[i] && dp[i] < dp[j] + arr[i]) {
-                dp[i] = dp[j] + arr[i];
+            if(arr[j] > arr[i] && dp[i] < dp[j] + 1) {
+                dp[i] = dp[j] + 1;
             }
         }
         if(max < dp[i]) max = dp[i];

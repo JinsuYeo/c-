60a368e14b76c1cbc3ba464da9c75a636aae0fd7
JinsuYeo <68772263+JinsuYeo@users.noreply.github.com>
2022-01-18 10:29:28 +0900

    동적 프로그래밍 연습 백준 1912번
    연속되는 수의 합 중 가장 큰 수 찾기.
    처음에는 음수가 나오면 dp를 초기화 하려 했으나 2 3 -3 4 처럼 음수를 포함해야 가장 큰 수(6)이 나오는 경우가 있으므로 수정함.
    dp[i-1] + arr[i]가 arr[i] 보다 작은 경우 dp[i]를 arr[i]로 초기화해줘서 문제 풀음.
    25분, O(n)

--- cpp algorithm/cpp algorithm/main.cpp
+++ cpp algorithm/cpp algorithm/main.cpp
@@ -3,44 +3,28 @@
 
 using namespace std;
 
-int dpl[1001], dpr[1001], arr[1001], maxarr[1001];
+int dp[100001], arr[100001];
 
 int main(int argc, const char * argv[]) {
-    int n{}, result{1};
+    int n{}, result{};
     cin >> n;
     for (int i {1}; i <= n; i++) {
         cin >> arr[i];
     }
     
-    dpl[1] = 1;
-    maxarr[1] = 1;
-    dpr[n] = 1;
-    maxarr[n] = 1;
+    dp[1] = arr[1];
+    result = arr[1];
     
-    for(int i {2}; i <= n; i++) {
-        dpl[i] = 1;
-        for(int j{1}; j <= i-1; j++) {
-            if(arr[j] < arr[i] && dpl[i] < dpl[j] + 1) {
-                dpl[i] = dpl[j] + 1;
-            }
+    for(int i{2}; i <= n; i++) {
+        if(dp[i-1] + arr[i] > arr[i]) {
+            dp[i] = dp[i-1] + arr[i];
+        } else {
+            dp[i] = arr[i];
         }
-        maxarr[i] = dpl[i];
-    }
-    
-    for(int i{n-1}; i >= 1; i--) {
-        dpr[i] = 1;
-        for(int k{n}; k > i; k--) {
-            if(arr[k] < arr[i] && dpr[i] < dpr[k] + 1) {
-                dpr[i] = dpr[k] + 1;
-            }
-        }
-        maxarr[i] += dpr[i]-1;
     }
     
     for(int i{1}; i <= n; i++) {
-        if(result < maxarr[i]) {
-            result = maxarr[i];
-        }
+        result = result < dp[i] ? dp[i] : result;
         
     }
     

654e05003c3dc480867c97fcee89492f811f031e
JinsuYeo <68772263+JinsuYeo@users.noreply.github.com>
2022-01-19 17:02:21 +0900

    접근 수정자
     protected class member는 base class의 모든 메서드에서 접근 가능. base class에서 파생된 클래스에서도 접근 가능. but base, derived class에서 만들어진 객체에서는 접근 불가능(private 처럼).
     private member는 파생된 클래스에서도 접근이 불가능하지만 상속은 됨.
    base class의 friend 클래스, 함수들은
     base class의 public, private, protect 모두에 접근할 수 있음.
    base class의 객체는 public 멤버만 접근 가능.
    파생 클래스의 friend는 파생 클래스가 접근 가능한 멤버들에 대해서만 접근 가능.

--- Inheritance/Inheritance/main.cpp
+++ Inheritance/Inheritance/main.cpp
@@ -4,34 +4,78 @@
 
 using namespace std;
 
+class Base {
+    // Note friend of Base have access to all
+public:
+    int a{};
+    void display() { cout << a << ", " << b << ", " << c << endl;} // member method has access to all
+protected:
+    int b{};
+private:
+    int c{};
+};
+
+class Derived: public Base {
+    // Note friends of Derived have access to only Derived has access to
+    
+    // a will be public
+    // b will be protected
+    // c will not be acceessible
+public:
+    void access_base_members(){
+        a = 100; // OK
+        b = 200; // OK
+//        c = 300; // not accessible
+    }
+};
+
 int main(int argc, const char * argv[]) {
-    cout << "\n=== Account ===================" << endl;
-    Account acc{};
-    acc.deposit(2000.0);
-    acc.withdraw(500.0);
     
-    cout << endl;
+    cout << "=========== Base member access from base object =============\n";
+    Base base;
+    base.a = 100;
+//    base.b = 200; // Compiler error
+//    base.c = 300; // Compiler error
     
-    Account *p_acc {nullptr};
-    p_acc = new Account();
-    p_acc->deposit(1000.0);
-    p_acc->withdraw(500.0);
-    delete p_acc;
     
-    cout << "\n=== Savings Account ===================" << endl;
-    Savings_Account sav_acc{};
-    sav_acc.deposit(2000.0);
-    sav_acc.withdraw(500.0);
+    cout << "=========== Derived member access from derived object =============\n";
+    Derived d;
+    d.a = 100;
+    //d.b = 200; // Compiler error
+    //d.c = 300; // Compiler error
     
-    cout << endl;
     
-    Savings_Account *p_sav_acc {nullptr};
-    p_sav_acc = new Savings_Account();
-    p_sav_acc->deposit(1000.0);
-    p_sav_acc->withdraw(500.0);
-    delete p_sav_acc;
     
-    cout << "\n======================================" << endl;
+    
+    
+    
+//    cout << "\n=== Account ===================" << endl;
+//    Account acc{};
+//    acc.deposit(2000.0);
+//    acc.withdraw(500.0);
+//
+//    cout << endl;
+//
+//    Account *p_acc {nullptr};
+//    p_acc = new Account();
+//    p_acc->deposit(1000.0);
+//    p_acc->withdraw(500.0);
+//    delete p_acc;
+//
+//    cout << "\n=== Savings Account ===================" << endl;
+//    Savings_Account sav_acc{};
+//    sav_acc.deposit(2000.0);
+//    sav_acc.withdraw(500.0);
+//
+//    cout << endl;
+//
+//    Savings_Account *p_sav_acc {nullptr};
+//    p_sav_acc = new Savings_Account();
+//    p_sav_acc->deposit(1000.0);
+//    p_sav_acc->withdraw(500.0);
+//    delete p_sav_acc;
+//
+//    cout << "\n======================================" << endl;
     
     return 0;
 }

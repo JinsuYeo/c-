784469b2739a82605a98943b981196de439527f9
JinsuYeo <68772263+JinsuYeo@users.noreply.github.com>
2022-01-22 13:26:04 +0900

    다형성 예시
    가상 함수 사용x 시 정적 바인딩 되어 Base 클래스 포인터에 파생 클래스 객체를 넣어 메서드 사용 시 Base의 메서드가 호출됨. redefine, 오버로딩은 정적 바인딩.

--- polymorphism/polymorphism/main.cpp
+++ polymorphism/polymorphism/main.cpp
@@ -1,37 +1,82 @@
 #include <iostream>
 
-class Base {
+//class Base {
+//public:
+//    void say_hello() const {
+//        std::cout << "Hello - I'm a Base class object" << std::endl;
+//    }
+//};
+//
+//class Derived: public Base {
+//public:
+//    void say_hello() const {
+//        std::cout << "Hello - I'm a Derived class object" << std::endl;
+//
+//    }
+//};
+//
+//void greeting(const Base &obj){
+//    std::cout << "Greetings: ";
+//    obj.say_hello();
+//}
+
+class Account {
 public:
-    void say_hello() const {
-        std::cout << "Hello - I'm a Base class object" << std::endl;
+    void withdraw(double amount) {
+        std::cout << "In Account::withdraw" << std::endl;
     }
 };
 
-class Derived: public Base {
+class Checking: public Account {
 public:
-    void say_hello() const {
-        std::cout << "Hello - I'm a Derived class object" << std::endl;
-        
+    void withdraw(double amount) {
+        std::cout << "In Checking::withdraw" << std::endl;
     }
 };
 
-void greeting(const Base &obj){
-    std::cout << "Greetings: ";
-    obj.say_hello();
-}
+class Savings: public Account {
+public:
+    void withdraw(double amount) {
+        std::cout << "In Savings::withdraw" << std::endl;
+    }
+};
+
+class Trust: public Account {
+public:
+    void withdraw(double amount) {
+        std::cout << "In Trust::withdraw" << std::endl;
+    }
+};
 
 int main(int argc, const char * argv[]) {
-    Base b;
-    b.say_hello();
-    greeting(b);
+    std::cout << "\n === Ponters ====" << std::endl;
+    Account *p1 = new Account();
+    Account *p2 = new Savings();
+    Account *p3 = new Checking();
+    Account *p4 = new Trust();
     
-    Derived d;
-    d.say_hello();
-    greeting(d);
+    p1->withdraw(1000);
+    p2->withdraw(1000);
+    p3->withdraw(1000);
+    p4->withdraw(1000);
     
-    Base *ptr = new Derived();
-    ptr->say_hello();
-    greeting(*ptr);
+    std::cout << "\n === Clean up ====" << std::endl;
+    delete p1;
+    delete p2;
+    delete p3;
+    delete p4;
+    
+//    Base b;
+//    b.say_hello();
+//    greeting(b);
+//
+//    Derived d;
+//    d.say_hello();
+//    greeting(d);
+//    
+//    Base *ptr = new Derived();
+//    ptr->say_hello();
+//    greeting(*ptr);
     
     return 0;
 }

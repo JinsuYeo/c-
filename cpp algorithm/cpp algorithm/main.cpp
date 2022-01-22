cd38ea4d057476ec2b5649a6ca0f6cf1034ddb92
JinsuYeo <68772263+JinsuYeo@users.noreply.github.com>
2022-01-22 20:00:51 +0900

    정렬 연습 백준 2751번
    정렬 공부 필요. 처음에는 삽입 정렬로 구현 했다가 시간 초과 나서 sort()함수로 구현.
    sort() 함수도 사용 방법 몰라 찾아 봤었고 정렬 방법에 따른 시간 복잡도 등 공부하고, sort함수 사용하지 않고 직접 구현해서 풀어볼 것.

--- cpp algorithm/cpp algorithm/main.cpp
+++ cpp algorithm/cpp algorithm/main.cpp
@@ -1,28 +1,28 @@
-#include <iostream>
+#include <cstdio>
 #include <algorithm>
 
 using namespace std;
 
-long dp[1001], arr[5001];
+int arr[1000000];
+
+bool compare(int a, int b) {
+    return a < b;
+}
 
 int main(int argc, const char * argv[]) {
     int n{};
-    cin >> n;
+    scanf("%d", &n);
     
-    for(int i{1}; i <= n; i++) {
-        cin >> arr[i];
+    for(int i{}; i < n; i++) {
+        scanf("%d", &arr[i]);
     }
     
-    dp[1] = arr[1];
+    sort(&arr[0], &arr[n], compare);
+
     
-    for(int i{2}; i <= n; i++) {
-        dp[i] = arr[i];
-        for(int j{1}; j <= i; j++) {
-            dp[i] = max(dp[i], dp[i-j] + arr[j]);
-        }
+    for(int i{}; i < n; i++) {
+        printf("%d\n", arr[i]);
     }
-    
-    cout << dp[n] << endl;
     
     return 0;
 }

6b7728727f325bab6e0f46c215c1fbd33155bcbf
JinsuYeo <68772263+JinsuYeo@users.noreply.github.com>
2022-01-22 15:44:30 +0900

    Base class reference
    기반 클래스 참조는 함수에 객체를 전달할 때 유용. 기반 클래스 참조 사용하여 다형성 함수 호출 가능.

--- polymorphism/polymorphism/main.cpp
+++ polymorphism/polymorphism/main.cpp
@@ -1,66 +1,91 @@
 #include <iostream>
 
-class Base {
+//class Base {
+//public:
+//    virtual void say_hello() const {
+//        std::cout << "Hello - I'm a Base class object" << std::endl;
+//    }
+//};
+//
+//class Derived: public Base {
+//public:
+//    virtual void say_hello() const override{
+//        std::cout << "Hello - I'm a Derived class object" << std::endl;
+//
+//    }
+//};
+//
+//void greeting(const Base &obj){
+//    std::cout << "Greetings: ";
+//    obj.say_hello();
+//}
+//
+
+class Account {
 public:
-    virtual void say_hello() const {
-        std::cout << "Hello - I'm a Base class object" << std::endl;
+    virtual void withdraw(double amount) {
+        std::cout << "In Account::withdraw" << std::endl;
     }
+    virtual ~Account(){ std::cout << "Account::destructor" << std::endl; }
 };
 
-class Derived: public Base {
+class Checking: public Account {
 public:
-    virtual void say_hello() const override{
-        std::cout << "Hello - I'm a Derived class object" << std::endl;
-
+    virtual void withdraw(double amount) {
+        std::cout << "In Checking::withdraw" << std::endl;
     }
+    virtual ~Checking(){ std::cout << "Checking::destructor" << std::endl; }
 };
 
-void greeting(const Base &obj){
-    std::cout << "Greetings: ";
-    obj.say_hello();
+class Savings: public Account {
+public:
+    virtual void withdraw(double amount) {
+        std::cout << "In Savings::withdraw" << std::endl;
+    }
+    virtual ~Savings(){ std::cout << "Savings::destructor" << std::endl; }
+};
+
+class Trust: public Account {
+public:
+    virtual void withdraw(double amount) {
+        std::cout << "In Trust::withdraw" << std::endl;
+    }
+    virtual ~Trust(){ std::cout << "Trust::destructor" << std::endl; }
+};
+
+void do_withdraw(Account &account, double amount) {
+    account.withdraw(amount);
 }
-//
-//class Account {
-//public:
-//    virtual void withdraw(double amount) {
-//        std::cout << "In Account::withdraw" << std::endl;
-//    }
-//    virtual ~Account(){ std::cout << "Account::destructor" << std::endl; }
-//};
-//
-//class Checking: public Account {
-//public:
-//    virtual void withdraw(double amount) {
-//        std::cout << "In Checking::withdraw" << std::endl;
-//    }
-//    virtual ~Checking(){ std::cout << "Checking::destructor" << std::endl; }
-//};
-//
-//class Savings: public Account {
-//public:
-//    virtual void withdraw(double amount) {
-//        std::cout << "In Savings::withdraw" << std::endl;
-//    }
-//    virtual ~Savings(){ std::cout << "Savings::destructor" << std::endl; }
-//};
-//
-//class Trust: public Account {
-//public:
-//    virtual void withdraw(double amount) {
-//        std::cout << "In Trust::withdraw" << std::endl;
-//    }
-//    virtual ~Trust(){ std::cout << "Trust::destructor" << std::endl; }
-//};
 
 int main(int argc, const char * argv[]) {
-    Base *p1 = new Base();
-    p1->say_hello();
+    Account a;
+    Account &ref = a;
+    ref.withdraw(1000);
     
-    Derived *p2 = new Derived();
-    p2->say_hello();
+    Trust t;
+    Account &ref1 = t;
+    ref1.withdraw(1000);
     
-    Base *p3 = new Derived();
-    p3->say_hello();
+    Account a1;
+    Savings a2;
+    Checking a3;
+    Trust a4;
+    
+    do_withdraw(a1, 1000);
+    do_withdraw(a2, 2000);
+    do_withdraw(a3, 3000);
+    do_withdraw(a4, 4000);
+    
+    
+    
+//    Base *p1 = new Base();
+//    p1->say_hello();
+//
+//    Derived *p2 = new Derived();
+//    p2->say_hello();
+//
+//    Base *p3 = new Derived();
+//    p3->say_hello();
     
     
 //    std::cout << "\n === Ponters ====" << std::endl;

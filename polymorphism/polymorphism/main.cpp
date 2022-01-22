e0645926a26b6e1dd6d01675ffee818f0b219c86
JinsuYeo <68772263+JinsuYeo@users.noreply.github.com>
2022-01-22 19:02:24 +0900

    인터페이스
    인터페이스 클래스는 순수 가상 함수만을 가지는 클래스임. 클래스 사용자가 사용할 수 있는 일반적인 서비스 set을 제공함.
    순수 가상 함수를 public으로 선언해야 유용함.
    클래스가 concrete 되거나 인터페이스 클레스에서 제공되는 함수들을 이용하기 위해서는 모든 순수 가상 함수를 구현해야 함.
    Java, c# 등의 언어와 다르게 c++에서는 인터페이스를 만드는 특정한 키워드나 방법이 없음. 순수 가상 함수가 있는 추상 클래스를 사용해 인터페이스의 개념을 달성해야 함.
    I를 클래스 이름 앞에 붙이면 클래스가 인터페이스로 이용되었다는 것을 알려줌.
    추상 클래스에서 순수 가상 함수를 사용하는 것과 큰 차이 없음.
    friend 함수는 상속되지 않음.
    -> 만약 멤버 메서드 이용할 수 없는 경우(ex) operator<<, friend 함수를 이용하는 경우), 상속, 가상 함수 사용이 불가능 함. -> 각 (파생)클래스마다 일일히 함수 redefine, 오버로딩 필요 + 기반 클래스 포인터 이용 시 동적 바인딩 불가능.
    -> 별도의 인터페이스를 사용하여 문제 해결 가능(ex) 인터페이스를 기반 클래스가 상속).

--- polymorphism/polymorphism/main.cpp
+++ polymorphism/polymorphism/main.cpp
@@ -22,122 +22,186 @@
 //}
 //
 //
-//class Account {
-//public:
-//    virtual void withdraw(double amount) {
-//        std::cout << "In Account::withdraw" << std::endl;
-//    }
-//    virtual ~Account(){ std::cout << "Account::destructor" << std::endl; }
-//};
-//
-//class Checking: public Account {
-//public:
-//    virtual void withdraw(double amount) {
-//        std::cout << "In Checking::withdraw" << std::endl;
-//    }
-//    virtual ~Checking(){ std::cout << "Checking::destructor" << std::endl; }
-//};
-//
-//class Savings: public Account {
-//public:
-//    virtual void withdraw(double amount) {
-//        std::cout << "In Savings::withdraw" << std::endl;
-//    }
-//    virtual ~Savings(){ std::cout << "Savings::destructor" << std::endl; }
-//};
-//
-//class Trust: public Account {
-//public:
-//    virtual void withdraw(double amount) {
-//        std::cout << "In Trust::withdraw" << std::endl;
-//    }
-//    virtual ~Trust(){ std::cout << "Trust::destructor" << std::endl; }
-//};
-//
-//void do_withdraw(Account &account, double amount) {
-//    account.withdraw(amount);
-//}
-
-class Shape {
+class I_Printable {
+    friend std::ostream &operator<<(std::ostream &os, const I_Printable &obj);
 public:
-    virtual void draw() = 0;
-    virtual void rotate() = 0;
-    virtual ~Shape(){}
+    virtual void print(std::ostream &os) const = 0;
 };
 
-class Open_Shape: public Shape {
-public:
-    virtual ~Open_Shape(){}
-};
-
-class Closed_Shape: public Shape {
-public:
-    virtual ~Closed_Shape(){}
-};
-
-class Line: public Open_Shape {
-public:
-    virtual void draw() override {
-        std::cout << "Drawing a line" << std::endl;
-    }
-    virtual void rotate() override {
-        std::cout << "Rotating a line" << std::endl;
-    }
-    virtual ~Line(){}
-};
-
-class Circle: public Closed_Shape {
-public:
-    virtual void draw() override {
-        std::cout << "Drawing a circle" << std::endl;
-    }
-    virtual void rotate() override {
-        std::cout << "Rotating a circle" << std::endl;
-    }
-    virtual ~Circle() {}
-};
-
-class Square: public Closed_Shape {
-public:
-    virtual void draw() override {
-        std::cout << "Drawing a square" << std::endl;
-    }
-    virtual void rotate() override {
-        std::cout << "Rotating a square" << std::endl;
-    }
-    virtual ~Square() {}
-};
-
-void screen_refresh(const std::vector<Shape*> &shapes){
-    std::cout << "Refreshing" << std::endl;
-    for(const auto p: shapes)
-        p->draw();
+std::ostream &operator<<(std::ostream &os, const I_Printable &obj){
+    obj.print(os);
+    return os;
 }
 
-int main(int argc, const char * argv[]) {
-    Circle c;
-    c.draw();
-    
-    Shape *ptr = new Circle();
-    ptr->draw();
-    ptr->rotate();
-    delete ptr;
-    
-    Shape *s1 = new Circle();
-    Shape *s2 = new Line();
-    Shape *s3 = new Square();
-    
-    std::vector<Shape*> shapes {s1, s2, s3};
+class Account: public I_Printable {
+//    friend std::ostream &operator<<(std::ostream &os, const Account &acc);
+public:
+    virtual void withdraw(double amount) {
+        std::cout << "In Account::withdraw" << std::endl;
+    }
+    virtual void print(std::ostream &os) const override {
+        os << "Account display";
+    }
+    virtual ~Account(){ std::cout << "Account::destructor" << std::endl; }
+};
+
+//std::ostream &operator<<(std::ostream &os, const Account &acc){
+//    os << "Account display";
+//    return os;
+//}
+
+class Checking: public Account {
+//    friend std::ostream &operator<<(std::ostream &os, const Checking &acc);
+public:
+    virtual void withdraw(double amount) override {
+        std::cout << "In Checking::withdraw" << std::endl;
+    }
+    virtual void print(std::ostream &os) const override {
+        os << "Checking display";
+    }
+    virtual ~Checking(){ std::cout << "Checking::destructor" << std::endl; }
+};
+
+//std::ostream &operator<<(std::ostream &os, const Checking &acc){
+//    os << "Checking display";
+//    return os;
+//}
+
+class Savings: public Account {
+//    friend std::ostream &operator<<(std::ostream &os, const Savings &acc);
+public:
+    virtual void withdraw(double amount) override {
+        std::cout << "In Savings::withdraw" << std::endl;
+    }
+    virtual void print(std::ostream &os) const override {
+        os << "Savings display";
+    }
+    virtual ~Savings(){ std::cout << "Savings::destructor" << std::endl; }
+};
+//
+//std::ostream &operator<<(std::ostream &os, const Savings &acc){
+//    os << "Savings display";
+//    return os;
+//}
+
+class Trust: public Account {
+//    friend std::ostream &operator<<(std::ostream &os, const Trust &acc);
+public:
+    virtual void withdraw(double amount) override {
+        std::cout << "In Trust::withdraw" << std::endl;
+    }
+    virtual void print(std::ostream &os) const override {
+        os << "Trust display";
+    }
+    virtual ~Trust(){ std::cout << "Trust::destructor" << std::endl; }
+};
+
+//std::ostream &operator<<(std::ostream &os, const Trust &acc){
+//    os << "Trust display";
+//    return os;
+//}
+
+void do_withdraw(Account &account, double amount) {
+    account.withdraw(amount);
+}
+//
+//class Shape {
+//public:
+//    virtual void draw() = 0;
+//    virtual void rotate() = 0;
+//    virtual ~Shape(){}
+//};
+//
+//class Open_Shape: public Shape {
+//public:
+//    virtual ~Open_Shape(){}
+//};
+//
+//class Closed_Shape: public Shape {
+//public:
+//    virtual ~Closed_Shape(){}
+//};
+//
+//class Line: public Open_Shape {
+//public:
+//    virtual void draw() override {
+//        std::cout << "Drawing a line" << std::endl;
+//    }
+//    virtual void rotate() override {
+//        std::cout << "Rotating a line" << std::endl;
+//    }
+//    virtual ~Line(){}
+//};
+//
+//class Circle: public Closed_Shape {
+//public:
+//    virtual void draw() override {
+//        std::cout << "Drawing a circle" << std::endl;
+//    }
+//    virtual void rotate() override {
+//        std::cout << "Rotating a circle" << std::endl;
+//    }
+//    virtual ~Circle() {}
+//};
+//
+//class Square: public Closed_Shape {
+//public:
+//    virtual void draw() override {
+//        std::cout << "Drawing a square" << std::endl;
+//    }
+//    virtual void rotate() override {
+//        std::cout << "Rotating a square" << std::endl;
+//    }
+//    virtual ~Square() {}
+//};
+//
+//void screen_refresh(const std::vector<Shape*> &shapes){
+//    std::cout << "Refreshing" << std::endl;
 //    for(const auto p: shapes)
 //        p->draw();
-    screen_refresh(shapes);
+//}
+//
+int main(int argc, const char * argv[]) {
+//    Account a;
+//    std::cout << a << std::endl;
+//
+//    Checking c;
+//    std::cout << c << std::endl;
+//
+//    Savings s;
+//    std::cout << s << std::endl;
+//
+//    Trust t;
+//    std::cout << t << std::endl;
+//
+    Account *p1 = new Account();
+    std::cout << *p1 << std::endl;
     
-    delete s1;
-    delete s2;
-    delete s3;
+    Account *p2 = new Checking();
+    std::cout << *p2 << std::endl;
     
     
-    
+//    Circle c;
+//    c.draw();
+//
+//    Shape *ptr = new Circle();
+//    ptr->draw();
+//    ptr->rotate();
+//    delete ptr;
+//
+//    Shape *s1 = new Circle();
+//    Shape *s2 = new Line();
+//    Shape *s3 = new Square();
+//
+//    std::vector<Shape*> shapes {s1, s2, s3};
+////    for(const auto p: shapes)
+////        p->draw();
+//    screen_refresh(shapes);
+//
+//    delete s1;
+//    delete s2;
+//    delete s3;
+
     
     
 //    Account a;

ec25e9470724231d2130bc580e54ffec279cada4
JinsuYeo <68772263+JinsuYeo@users.noreply.github.com>
2022-01-22 14:38:22 +0900

    c++11 override specifier
    만약 파생 클래스에서 기반 클래스의 가상 함수를 override 할 때 실수로 반환 타입이나 이름을 변견ㅇ 시 override 되지 않고 redefine 됨(정적 바인딩 됨). 이것은 찾기 힘든 오류.
    c++11 override specifier로 이러한 문제 해결 가능.
    파생 클래스의 메서드(override 하는) 뒤에 override 키워드 붙여 컴파일러에게 override 할 것임을 알려줌 -> override 되는 지 확인하고 안될 시 컴파일 에러 발생시킴.

--- polymorphism/polymorphism/main.cpp
+++ polymorphism/polymorphism/main.cpp
@@ -1,74 +1,84 @@
 #include <iostream>
 
-//class Base {
+class Base {
+public:
+    virtual void say_hello() const {
+        std::cout << "Hello - I'm a Base class object" << std::endl;
+    }
+};
+
+class Derived: public Base {
+public:
+    virtual void say_hello() const override{
+        std::cout << "Hello - I'm a Derived class object" << std::endl;
+
+    }
+};
+
+void greeting(const Base &obj){
+    std::cout << "Greetings: ";
+    obj.say_hello();
+}
+//
+//class Account {
 //public:
-//    void say_hello() const {
-//        std::cout << "Hello - I'm a Base class object" << std::endl;
+//    virtual void withdraw(double amount) {
+//        std::cout << "In Account::withdraw" << std::endl;
 //    }
+//    virtual ~Account(){ std::cout << "Account::destructor" << std::endl; }
 //};
 //
-//class Derived: public Base {
+//class Checking: public Account {
 //public:
-//    void say_hello() const {
-//        std::cout << "Hello - I'm a Derived class object" << std::endl;
-//
+//    virtual void withdraw(double amount) {
+//        std::cout << "In Checking::withdraw" << std::endl;
 //    }
+//    virtual ~Checking(){ std::cout << "Checking::destructor" << std::endl; }
 //};
 //
-//void greeting(const Base &obj){
-//    std::cout << "Greetings: ";
-//    obj.say_hello();
-//}
-
-class Account {
-public:
-    virtual void withdraw(double amount) {
-        std::cout << "In Account::withdraw" << std::endl;
-    }
-    virtual ~Account(){ std::cout << "Account::destructor" << std::endl; }
-};
-
-class Checking: public Account {
-public:
-    virtual void withdraw(double amount) {
-        std::cout << "In Checking::withdraw" << std::endl;
-    }
-    virtual ~Checking(){ std::cout << "Checking::destructor" << std::endl; }
-};
-
-class Savings: public Account {
-public:
-    virtual void withdraw(double amount) {
-        std::cout << "In Savings::withdraw" << std::endl;
-    }
-    virtual ~Savings(){ std::cout << "Savings::destructor" << std::endl; }
-};
-
-class Trust: public Account {
-public:
-    virtual void withdraw(double amount) {
-        std::cout << "In Trust::withdraw" << std::endl;
-    }
-    virtual ~Trust(){ std::cout << "Trust::destructor" << std::endl; }
-};
+//class Savings: public Account {
+//public:
+//    virtual void withdraw(double amount) {
+//        std::cout << "In Savings::withdraw" << std::endl;
+//    }
+//    virtual ~Savings(){ std::cout << "Savings::destructor" << std::endl; }
+//};
+//
+//class Trust: public Account {
+//public:
+//    virtual void withdraw(double amount) {
+//        std::cout << "In Trust::withdraw" << std::endl;
+//    }
+//    virtual ~Trust(){ std::cout << "Trust::destructor" << std::endl; }
+//};
 
 int main(int argc, const char * argv[]) {
-    std::cout << "\n === Ponters ====" << std::endl;
-    Account *p1 = new Account();
-    Account *p2 = new Savings();
-    Account *p3 = new Checking();
-    Account *p4 = new Trust();
+    Base *p1 = new Base();
+    p1->say_hello();
     
-    p1->withdraw(1000);
-    p2->withdraw(1000);
-    p3->withdraw(1000);
-    p4->withdraw(1000);
+    Derived *p2 = new Derived();
+    p2->say_hello();
     
-    std::cout << "\n === Clean up ====" << std::endl;
-    delete p1;
-    delete p2;
-    delete p3;
-    delete p4;
+    Base *p3 = new Derived();
+    p3->say_hello();
+    
+    
+//    std::cout << "\n === Ponters ====" << std::endl;
+//    Account *p1 = new Account();
+//    Account *p2 = new Savings();
+//    Account *p3 = new Checking();
+//    Account *p4 = new Trust();
+//
+//    p1->withdraw(1000);
+//    p2->withdraw(1000);
+//    p3->withdraw(1000);
+//    p4->withdraw(1000);
+//
+//    std::cout << "\n === Clean up ====" << std::endl;
+//    delete p1;
+//    delete p2;
+//    delete p3;
+//    delete p4;
     
 //    Base b;
 //    b.say_hello();

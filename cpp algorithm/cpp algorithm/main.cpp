0ec50444606c4657f29f93e37de2ac34b435b9d1
JinsuYeo <68772263+JinsuYeo@users.noreply.github.com>
2022-01-24 14:47:24 +0900

    퀵정렬
    대표적인 분할정복 알고리즘. 평균속도 O(n*logn). 하나의 큰 문제를 두개의 작은 문제로 분할하는 식으로 빠르게 정렬. 즉, 특정한 값을 기준으로 큰 숫자와 작은 숫자를 서로 교환한 뒤에 배열을 반으로 나눔.
    기준값 피벗(pivot)이 있음. 보통 첫번째 원소를 피벗 값으로 설정하고 사용한다.
    ex) 1 3 5 2 4
          1 3 5 2 4 // 왼쪽부터 오른쪽으로 1보다 큰 수, 오른쪽부터 왼쪽으로 1보다 작은 수 찾았을 때, 1보다 큰 수 없으므로 1이 1과 교환된 후 1보다 작은수, 큰수로 나뉘어져 각각의 첫번째 값이 피벗이 됨. 여기서는 1보다 작은 수 없으니 3이 1보다 큰 수의 배열의 피벗이 되어 과정 반복한다.
        1 3 5 2 4 // 3보다 큰 수중 왼쪽에서 첫번째 수인 5와 3보다 작은 수 중 오른쪽에서 첫번째인 2가 교환된다.
        1 3 2 5 4 //3보다 오른쪽에서 왼쪽으로 탐색한 3보다 작은 수인 2가 왼쪽에서 오른쪽으로 3보다 큰 수인 5와 엇갈렸으므로, 2와 3을 바꿔준다.
       1 2 3 5 4 // 이 때 피벗이었던 3을 기준으로 왼쪽은 3보다 작은 수, 오른쪽은 3보다 큰수라는 특징 가진다. 다시 1(3의 왼쪽 수중 가장 왼쪽 수)과 5(3의 오른쪽 수 중 가장 왼쪽 수)를 피벗으로 이 과정을 반복한다.
    재귀함수로 구현하는 경우가 많으며, 퀵정렬의 최악 시간복잡도는 O(n^2)이다.
    ex) 1 2 3 4 5 6처럼 이미 정렬된 경우. 이럴 떈 삼입정렬 사용하여 빠르게 처리가 가능하다.
    -> 정렬할 데이터의 특성에 따라 적절한 정렬 알고리즘 사용하는 것이 중요.
    참고) 백준 2751번은 퀵정렬 사용해도 이러한 이유로 시간초과 발생함. 병합정렬이나 힙정렬을 사용해야 한다고 함.

--- cpp algorithm/cpp algorithm/main.cpp
+++ cpp algorithm/cpp algorithm/main.cpp
@@ -5,23 +5,40 @@ using namespace std;
 
 int arr[1000000];
 
+void quickSort(int* arr, int start, int end) {
+    if(start < end) {
+        int key{start}, i{start+1}, j{end}, temp{};
+        while(i <= j) {
+            while(i <= end && arr[i] <= arr[key])
+                i++;
+            while(j > start && arr[j] >= arr[key])
+                j--;
+            
+            if(i > j) {
+                temp = arr[j];
+                arr[j] = arr[key];
+                arr[key] = temp;
+            } else {
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        quickSort(arr, start, j-1);
+        quickSort(arr, j+1, end);
+    }
+    return;
+}
+
 int main(int argc, const char * argv[]) {
-    int n{}, j{}, temp{};
+    int n{};
     scanf("%d", &n);
     
     for(int i{}; i < n; i++) {
         scanf("%d", &arr[i]);
     }
     
-    for(int i{}; i < n - 1; i++) {
-        j = i;
-        while (j >= 0 && arr[j] > arr[j + 1]) {
-            temp = arr[j];
-            arr[j] = arr[j+1];
-            arr[j+1] = temp;
-            j--;
-        }
-    }
+    quickSort(arr, 0, n-1);
     
     for(int i{}; i < n; i++) {
         printf("%d\n", arr[i]);

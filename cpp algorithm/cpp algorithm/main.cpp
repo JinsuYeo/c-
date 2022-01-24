7443ff10c0dbef6519267837d06e17a78a62623d
JinsuYeo <68772263+JinsuYeo@users.noreply.github.com>
2022-01-24 11:31:16 +0900

    삽입 정렬
    시간 복잡도 O(n^2)을 가짐.
    각 숫자를 적절한 위치에 삽입하는 방식으로 문제를 해결. (무조건 위치를 바꾸는(반복을 수행하는) 방식인 선택 정렬, 버블 정렬과 다르게) '필요할 때'만 위치를 바꾸게(반복을 수행하게) 됨. -> 선택, 버블 정렬과 시간 복잡도는 같으나 더 빠름. 구현, 생각 어려운 편.
    ex) 1 3 5 2 4  // 앞에 원소들이 이미 정렬되었다고 가정함.
          1 3 5 2 4 //1 3 5 부분은 다시 정렬할 필요 없이 2만 앞의 숫자와 비교해서 올바른 위치에 삽입해주면 됨(자리 바꿔주면 됨).
          1 3 2 5 4
          1 2 3 5 4  // 5는 3보다 크므로 3보다 앞인 1, 2 와 비교할 필요 x(1~3은 이미 정렬되었다고 가정하기 때문에).
          1 2 3 4 5
    -> 기본적으로 정렬이 되어있다고 가정한다는 점에서 특정한 경우에 따라 굉장히 빠른 속도. 거의 정렬된 데이터 정렬 시 최고의 방법.

--- cpp algorithm/cpp algorithm/main.cpp
+++ cpp algorithm/cpp algorithm/main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int arr[1000000];
 
 int main(int argc, const char * argv[]) {
-    int n{}, temp{};
+    int n{}, j{}, temp{};
     scanf("%d", &n);
     
     for(int i{}; i < n; i++) {
@@ -14,12 +14,12 @@ int main(int argc, const char * argv[]) {
     }
     
     for(int i{}; i < n - 1; i++) {
-        for(int j{}; j < n - 1 - i; j++) {
-            if(arr[j] > arr[j + 1]) {
-                temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
+        j = i;
+        while (j >= 0 && arr[j] > arr[j + 1]) {
+            temp = arr[j];
+            arr[j] = arr[j+1];
+            arr[j+1] = temp;
+            j--;
         }
     }
     

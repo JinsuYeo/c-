3f5a0cea5029ad7cd77e04d09362f6ee62a47e13
JinsuYeo <68772263+JinsuYeo@users.noreply.github.com>
2022-01-24 23:50:45 +0900

    shared pointer 공유 포인터
    힙 객체의 소유권을 공유함. 유니크 포인터와 마찬가지로 shared pointer는 힙 객체 가리키나  그 힙 객체는 많은 공유 포인터 사이에서 공유될 수 있음. -> 복사, 할당 가능 <- 동일한 힙 객체에 접근할 수 있게 하는 방법임. 이동 symentic도 지원함.
    유니크 포인터 사용하여 힙에 객체 배열 할당 가능. but 공유 포인터는 기본적으로 이것을 지원하지 않음.
    공유된 힙 객체가 언제 삭제되어야 하는지 어떻게 알 수 있는가? -> 가장 일반적인 기술은 참조 카운팅을 사용하는 것. 공유 포인터 객체를 인스턴스화 할 때마다 힙 객체를 가리키거나 참조하도록 하면 카운터 증가. 이 카운터는 단순히 해당 힙 객체를 참조하는 공유 포인터의 수 가지고 있음.
    -> 참조 카운터가 0이 되면 안전하게 삭제 될 수 있다는 것 알 수 있음. 간단하며 효과적인 모델임.
    생성하는 방법은 고유 포인터와 거의 동일.use_count() 메서드는 현재 힙 객체 참조하는 공유 포인터 객체의 수를 반환함.
    make_shared 함수는 c++11부터 도입됨.

--- SmartPointer/SmartPointer/main.cpp
+++ SmartPointer/SmartPointer/main.cpp
@@ -19,6 +19,16 @@ public:
 };
 
 int main() {
+    std::shared_ptr<int> p1 {new int {100}};
+    std::cout << "Use count: " << p1.use_count() << std::endl;
+    
+    std::shared_ptr<int> p2 {p1};
+    std::cout << "Use count: " << p2.use_count() << std::endl;
+    
+    p1.reset();
+    std::cout << "Use count: " << p1.use_count() << std::endl;
+    std::cout << "Use count: " << p2.use_count() << std::endl;
+    
 //    Test *t1 = new Test {1000};
 //    delete t1;
 //
@@ -31,19 +41,19 @@ int main() {
 //        std::cout << "t2 is nullptr" << std::endl;
 //    }
     
-    std::unique_ptr<Account> a1 = std::make_unique<Checking_Account>("Moe", 5000);
-    std::cout << *a1 << std::endl;
-    a1->deposit(5000);
-    std::cout << *a1 << std::endl;
-    
-    std::vector<std::unique_ptr<Account>> accounts;
-    accounts.push_back(std::make_unique<Checking_Account>("James", 1000));
-    accounts.push_back(std::make_unique<Savings_Account>("Billy", 4000, 5.2));
-    accounts.push_back(std::make_unique<Trust_Account>("Bobby", 5000, 4.5));
-    
-    for(const auto &acc: accounts) {
-        std::cout << *acc << std::endl;
-    }
+//    std::unique_ptr<Account> a1 = std::make_unique<Checking_Account>("Moe", 5000);
+//    std::cout << *a1 << std::endl;
+//    a1->deposit(5000);
+//    std::cout << *a1 << std::endl;
+//
+//    std::vector<std::unique_ptr<Account>> accounts;
+//    accounts.push_back(std::make_unique<Checking_Account>("James", 1000));
+//    accounts.push_back(std::make_unique<Savings_Account>("Billy", 4000, 5.2));
+//    accounts.push_back(std::make_unique<Trust_Account>("Bobby", 5000, 4.5));
+//
+//    for(const auto &acc: accounts) {
+//        std::cout << *acc << std::endl;
+//    }
     
     return 0;
 }

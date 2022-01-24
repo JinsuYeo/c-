7ce349a8eb5ef24b6d7bc7cf20618670020dbe82
JinsuYeo <68772263+JinsuYeo@users.noreply.github.com>
2022-01-24 14:58:16 +0900

    정렬 연습 백준 2752번
    버블 정렬로 구현.

--- cpp algorithm/cpp algorithm/main.cpp
+++ cpp algorithm/cpp algorithm/main.cpp
@@ -3,42 +3,24 @@
 
 using namespace std;
 
-int arr[1000000];
-
-void quickSort(int* arr, int start, int end) {
-    if(start < end) {
-        int key{start}, i{start+1}, j{end}, temp{};
-        while(i <= j) {
-            while(i <= end && arr[i] <= arr[key])
-                i++;
-            while(j > start && arr[j] >= arr[key])
-                j--;
-            
-            if(i > j) {
-                temp = arr[j];
-                arr[j] = arr[key];
-                arr[key] = temp;
-            } else {
-                temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-            }
-        }
-        quickSort(arr, start, j-1);
-        quickSort(arr, j+1, end);
-    }
-    return;
-}
+int arr[3];
 
 int main(int argc, const char * argv[]) {
-    int n{};
-    scanf("%d", &n);
+    int n{3}, temp{};
     
     for(int i{}; i < n; i++) {
         scanf("%d", &arr[i]);
     }
     
-    quickSort(arr, 0, n-1);
+    for(int i{}; i < n-1; i++) {
+        for(int j{}; j < n - 1 - i; j++) {
+            if(arr[j] > arr[j+1]) {
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
     
     for(int i{}; i < n; i++) {
         printf("%d\n", arr[i]);

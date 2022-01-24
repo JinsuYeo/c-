743f1e21f0a76015db21797466ca29e1e85d5312
JinsuYeo <68772263+JinsuYeo@users.noreply.github.com>
2022-01-24 21:43:01 +0900

    유니크 포인터 응용
    유니크 포인터 사용해 동적 바인딩 이용. 스마트 포인터 사용해도 기반 클래스에서 가상함수 사용 안했다면 정적 바인딩 됨.

--- SmartPointer/SmartPointer/main.cpp
+++ SmartPointer/SmartPointer/main.cpp
@@ -19,16 +19,30 @@ public:
 };
 
 int main() {
-    Test *t1 = new Test {1000};
-    delete t1;
+//    Test *t1 = new Test {1000};
+//    delete t1;
+//
+//    std::unique_ptr<Test> t2 {new Test {100}};
+//    std::unique_ptr<Test> t3 = std::make_unique<Test> (1000);
+//
+//    std::unique_ptr<Test> t4;
+//    t4 = std::move(t2);
+//    if (!t2) {
+//        std::cout << "t2 is nullptr" << std::endl;
+//    }
     
-    std::unique_ptr<Test> t2 {new Test {100}};
-    std::unique_ptr<Test> t3 = std::make_unique<Test> (1000);
+    std::unique_ptr<Account> a1 = std::make_unique<Checking_Account>("Moe", 5000);
+    std::cout << *a1 << std::endl;
+    a1->deposit(5000);
+    std::cout << *a1 << std::endl;
     
-    std::unique_ptr<Test> t4;
-    t4 = std::move(t2);
-    if (!t2) {
-        std::cout << "t2 is nullptr" << std::endl;
+    std::vector<std::unique_ptr<Account>> accounts;
+    accounts.push_back(std::make_unique<Checking_Account>("James", 1000));
+    accounts.push_back(std::make_unique<Savings_Account>("Billy", 4000, 5.2));
+    accounts.push_back(std::make_unique<Trust_Account>("Bobby", 5000, 4.5));
+    
+    for(const auto &acc: accounts) {
+        std::cout << *acc << std::endl;
     }
     
     return 0;

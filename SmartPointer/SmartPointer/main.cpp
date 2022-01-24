f5d458955d9ed4dd830f73a4e038fb75ccf7424d
JinsuYeo <68772263+JinsuYeo@users.noreply.github.com>
2022-01-24 21:33:25 +0900

    유니크 포인터
    유니크 포인터는 스택에 생성됨. -> 범위 벗어날 때 가리키는 힙 메모리 자동으로 삭제됨. delete 필요 없음.
    std::move()를 이용해 유니크 포인터를 이동시키면 원래 포인터가 들어있던 값은 nullptr로 바뀌고 옮긴 유니크 포인터 객체에 포인터가 들어감.

--- SmartPointer/SmartPointer/main.cpp
+++ SmartPointer/SmartPointer/main.cpp
@@ -23,8 +23,13 @@ int main() {
     delete t1;
     
     std::unique_ptr<Test> t2 {new Test {100}};
-    
     std::unique_ptr<Test> t3 = std::make_unique<Test> (1000);
+    
+    std::unique_ptr<Test> t4;
+    t4 = std::move(t2);
+    if (!t2) {
+        std::cout << "t2 is nullptr" << std::endl;
+    }
     
     return 0;
 }

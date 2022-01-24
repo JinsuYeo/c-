e7927423e2bc0677ca01e553be7a7e9897d85f5f
JinsuYeo <68772263+JinsuYeo@users.noreply.github.com>
2022-01-24 10:57:33 +0900

    버블 정렬
    직관적인 해결 방법으로, 바로 가까이에 있는 두 숫자끼리 비교해서 당장 더 작은 수를 앞으로 보내주는 것을 반복. 즉, 옆에 있는 값과 비교해서 더 작은 값을 반복적으로 앞으로 보내는 정렬 방법.
    -> 구현 가장 쉽지만 가장 비효율적.
    1~n, 1~n-1, 1~n-2...1~2까지 비교. 뒤에 있는 원소가 밥복마다 하나씩 줄어듦. 밥복할 때 마다 가장 큰 숫자가 가장 마지막 자리에 가게 됨.
    ex) 1 3 5 2 4
          1 3 5 2 4
          1 3 2 5 4
          1 3 2 4 5
          1 2 3 4 5
    데이터 개수 n개일 때, n*(n-1)/2번 연산. 시간복잡도 O(n^2)으로 선택정렬과 같으나 실제로는 더 느림 -> 버블 정렬은 매번 바꿔줘야 하지만 선택 정렬은 가장 작은 것 골라서 마지막에만 교체해줌. 가장 느린 정렬 방법임.

--- cpp algorithm/cpp algorithm/main.cpp
+++ cpp algorithm/cpp algorithm/main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int arr[1000000];
 
 int main(int argc, const char * argv[]) {
-    int n{}, min{INT_MAX}, index{}, temp{};
+    int n{}, temp{};
     scanf("%d", &n);
     
     for(int i{}; i < n; i++) {
@@ -14,18 +14,14 @@ int main(int argc, const char * argv[]) {
     }
     
     for(int i{}; i < n - 1; i++) {
-        min = INT_MAX;
-        for(int j{i}; j < n; j++) {
-            if(min > arr[j]) {
-                min = arr[j];
-                index = j;
+        for(int j{}; j < n - 1 - i; j++) {
+            if(arr[j] > arr[j + 1]) {
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
             }
         }
-        temp = arr[i];
-        arr[i] = arr[index];
-        arr[index] = temp;
     }
-
     
     for(int i{}; i < n; i++) {
         printf("%d\n", arr[i]);

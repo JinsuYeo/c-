ffc177cc6b51bee7233e682425265843dd66f22a
JinsuYeo <68772263+JinsuYeo@users.noreply.github.com>
2022-01-24 16:07:46 +0900

    정렬 연습 백준 2752번
    병합 정렬 이용해 구현.

--- cpp algorithm/cpp algorithm/main.cpp
+++ cpp algorithm/cpp algorithm/main.cpp
@@ -1,24 +1,56 @@
 #include <cstdio>
-#include <algorithm>
 
-int arr[3];
+int sorted[3];
+
+void merge(int arr[], int m, int middle, int n) {
+    int i = m;
+    int j = middle + 1;
+    int k = m;
+    while (i <= middle && j <= n) {
+        if(arr[i] <= arr[j]) {
+            sorted[k] = arr[i];
+            i++;
+        } else {
+            sorted[k] = arr[j];
+            j++;
+        }
+        k++;
+    }
+    if (i > middle) {
+        for(int t{j}; t <= n; t++) {
+            sorted[k] = arr[t];
+            k++;
+        }
+    } else {
+        for(int t{i}; t <= middle; t++) {
+            sorted[k] = arr[t];
+            k++;
+        }
+    }
+    
+    for(int t = m; t <= n; t++) {
+        arr[t] = sorted[t];
+    }
+}
+
+void mergeSort(int arr[], int m, int n) {
+    if(m < n) {
+        int middle = (m + n) / 2;
+        mergeSort(arr, m, middle);
+        mergeSort(arr, middle+1, n);
+        merge(arr, m, middle, n);
+    }
+}
 
 int main(int argc, const char * argv[]) {
-    int n{3}, j{}, temp{};
-    
+    int arr[3];
+    int n{3};
+
     for(int i{}; i < n; i++) {
         scanf("%d", &arr[i]);
     }
     
-    for(int i{}; i < n-1; i++) {
-        j = i;
-        while (j >= 0 && arr[j] > arr[j+1]) {
-            temp = arr[j];
-            arr[j] = arr[j+1];
-            arr[j+1] = temp;
-            j--;
-        }
-    }
+    mergeSort(arr, 0, n-1);
     
     for(int i{}; i < n; i++) {
         printf("%d\n", arr[i]);

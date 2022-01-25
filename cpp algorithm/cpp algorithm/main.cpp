a3a0c67478d68e7369b7ebed41c869104dc7fb8a
JinsuYeo <68772263+JinsuYeo@users.noreply.github.com>
2022-01-26 00:29:22 +0900

    계수 정렬
    범위 조건이 있는 경우에 한해서 굉장히 빠른 정렬 알고리즘.
    O(n)의 시간 복잡도 가짐. 크기를 기준으로 세는 알고리즘임.
    수들이 입력됐을 떄, 각각의 크기를 가진 수들의 개수를 센 후, 개수만큼 각각의 수를 출력하는 정렬 알고리즘.
    ex) 135543 들어오면 1 - 1개, 2 - 0개, 3 - 2개, 4 - 1개, 5 - 2개로 센 값을 저장한 후 각각의 수의 개수만큼 출력: 133455

--- cpp algorithm/cpp algorithm/main.cpp
+++ cpp algorithm/cpp algorithm/main.cpp
@@ -3,49 +3,29 @@
 
 using namespace std;
 
-int arr[1000000];
+int arr[3], cSort[1000001];
 
 int main() {
-    int n{};
-    cin >> n;
-    for(int i{}; i < n; i++) {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int n{3}, range{1000000};
+//    cin >> n;
+    for(int i{1}; i <= n; i++) {
         cin >> arr[i];
     }
     
-    for(int i{1}; i < n; i++) {
-        int c{i};
-        do {
-            int root = (c-1)/2;
-            if(arr[root] < arr[c]) {
-                int temp = arr[root];
-                arr[root] = arr[c];
-                arr[c] = temp;
+    for(int i{1}; i <= n; i++) {
+        cSort[arr[i]]++;
+    }
+    
+    for (int i{1}; i <= range; i++) {
+        if(cSort[i]) {
+            for(int j{}; j < cSort[i]; j++) {
+                cout << i << "\n";
             }
-            c = root;
         }
-        while(c != 0);
-    }
-    
-    for(int i{n-1}; i >= 0; i--) {
-        int temp = arr[0];
-        arr[0] = arr[i];
-        arr[i] = temp;
-        int root{};
-        int c{};
-        do {
-            c = root * 2 + 1;
-            if(c < i - 1 && arr[c] < arr[c+1]) c++;
-            if(c < i && arr[root] < arr[c]) {
-                temp = arr[root];
-                arr[root] = arr[c];
-                arr[c] = temp;
-            }
-            root = c;
-        } while (c < i);
-    }
-    
-    for (int i{}; i < n; i++) {
-        cout << arr[i] << "\n";
     }
     
     return 0;

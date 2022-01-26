b05cd9ff8bcbe86b3bec7027474a4ab95e40f7f8
JinsuYeo <68772263+JinsuYeo@users.noreply.github.com>
2022-01-26 10:31:52 +0900

    백준 10989번 계수정렬 수정
    코드 계수정렬로 구현한 것으로 수정. 이전 커밋은 char형 배열로 구현해서 실험해본 것임.

--- cpp algorithm/cpp algorithm/main.cpp
+++ cpp algorithm/cpp algorithm/main.cpp
@@ -5,24 +5,27 @@
 
 using namespace std;
 
-char arr[10000000];
+int arr[10001];
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
     
-    int n{};
+    int n{}, t{};
     cin >> n;
     
     for (int i{}; i < n; i++) {
-        cin >> arr[i];
+        cin >> t;
+        arr[t]++;
     }
     
-    sort(arr, arr+n);
-    
-    for (int i{}; i < n; i++) {
-        cout << arr[i] << "\n";
+    for (int i{1}; i <= 10000; i++) {
+        if(arr[i]){
+            for (int j{}; j < arr[i]; j++) {
+                cout << i << "\n";
+            }
+        }
     }
    
     return 0;

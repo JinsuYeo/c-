eafc7066729fcc40bedee8baca7bbd55e5bc0ced
JinsuYeo <68772263+JinsuYeo@users.noreply.github.com>
2022-01-26 10:29:49 +0900

    정렬 연습 백준 10989번
    메모리 제한이 8mb인데 입력 개수가 10000000이므로 int형 배열에 저장하기 힘듦(10000000*4/(1024*1024) = 약 38mb, char형으로 배열 선언해도 약 9.5mb임).
    입력값의 크기는 10000 이하의 자연수이므로 계수 정렬로 구현 시 배열 크기 10001만 사용하면 됨. 따라서 계수 정렬로 풀었더니 맞았음.

--- cpp algorithm/cpp algorithm/main.cpp
+++ cpp algorithm/cpp algorithm/main.cpp
@@ -5,42 +5,25 @@
 
 using namespace std;
 
-class Score{
-public:
-    string name;
-    int kor;
-    int eng;
-    int math;
-    Score(string s, int k, int e, int m)
-    : name{s}, kor{k}, eng{e}, math{m} {}
-};
-
-bool compare(Score a, Score b){
-    if(a.kor == b.kor && a.eng == b.eng && a.math == b.math) return a.name < b.name;
-    else if(a.kor == b.kor && a.eng == b.eng) return a.math > b.math;
-    else if(a.kor == b.kor) return a.eng < b.eng;
-    else return a.kor > b.kor;
-}
+char arr[10000000];
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
     
-    int n{}, k{}, e{}, m{};
-    string s;
-    vector<Score> v;
-    
+    int n{};
     cin >> n;
-    for(int i{}; i < n; i++) {
-        cin >> s >> k >> e >> m;
-        v.push_back(Score(s, k, e, m));
+    
+    for (int i{}; i < n; i++) {
+        cin >> arr[i];
     }
     
-    sort(v.begin(), v.end(), compare);
+    sort(arr, arr+n);
     
-    for (int i{}; i < n; i++)
-        cout << v.at(i).name << "\n";
-    
+    for (int i{}; i < n; i++) {
+        cout << arr[i] << "\n";
+    }
+   
     return 0;
 }

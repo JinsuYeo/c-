9a0d0fcb8c494a3c4787524bbcdea945a1077492
JinsuYeo <68772263+JinsuYeo@users.noreply.github.com>
2022-01-26 10:12:34 +0900

    정렬 연습 백준 10825번
    클래스와 sort함수 이용해 구현. compare함수로 점수가 같은 조건 모두 처리.

--- cpp algorithm/cpp algorithm/main.cpp
+++ cpp algorithm/cpp algorithm/main.cpp
@@ -1,32 +1,46 @@
 #include <iostream>
 #include <algorithm>
+#include <vector>
+#include <string>
 
 using namespace std;
 
-int arr[3], cSort[1000001];
+class Score{
+public:
+    string name;
+    int kor;
+    int eng;
+    int math;
+    Score(string s, int k, int e, int m)
+    : name{s}, kor{k}, eng{e}, math{m} {}
+};
+
+bool compare(Score a, Score b){
+    if(a.kor == b.kor && a.eng == b.eng && a.math == b.math) return a.name < b.name;
+    else if(a.kor == b.kor && a.eng == b.eng) return a.math > b.math;
+    else if(a.kor == b.kor) return a.eng < b.eng;
+    else return a.kor > b.kor;
+}
 
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
     
-    int n{3}, range{1000000};
-//    cin >> n;
-    for(int i{1}; i <= n; i++) {
-        cin >> arr[i];
+    int n{}, k{}, e{}, m{};
+    string s;
+    vector<Score> v;
+    
+    cin >> n;
+    for(int i{}; i < n; i++) {
+        cin >> s >> k >> e >> m;
+        v.push_back(Score(s, k, e, m));
     }
     
-    for(int i{1}; i <= n; i++) {
-        cSort[arr[i]]++;
-    }
+    sort(v.begin(), v.end(), compare);
     
-    for (int i{1}; i <= range; i++) {
-        if(cSort[i]) {
-            for(int j{}; j < cSort[i]; j++) {
-                cout << i << "\n";
-            }
-        }
-    }
+    for (int i{}; i < n; i++)
+        cout << v.at(i).name << "\n";
     
     return 0;
 }

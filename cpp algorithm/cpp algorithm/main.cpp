eea3f7ec0a7fed7744f37b7e9cbfd30b47f3f58f
JinsuYeo <68772263+JinsuYeo@users.noreply.github.com>
2022-01-27 10:29:51 +0900

    스택 연습 백준 9012번
    stack처럼 '('가 들어오면 쌓고 ')'가 들어오면 뺀다고 가정(스택 사이즈가 0이거나 스택의 마지막에 ')'가 있다면 ')'도 쌓음).
    stack의 사이즈가 0이 아니면(스택이 empty아니면) NO, empty면 YES 출력.

--- cpp algorithm/cpp algorithm/main.cpp
+++ cpp algorithm/cpp algorithm/main.cpp
@@ -10,40 +10,33 @@ int main() {
     cin.tie(NULL);
     cout.tie(NULL);
     
-    int n{}, t{};
+    int n{};
     string s;
     cin >> n;
     
-    vector<int> v;
+    vector<char> stack;
+    vector<string> v;
     
     for(int i{}; i < n; i++) {
         cin >> s;
-        if(s == "push") {
-            cin >> t;
-            v.push_back(t);
-        }
-        if(s == "top") {
-            if(v.size()) {
-                cout << v.back() << "\n";
-            } else {
-                cout << -1 << "\n";
-            }
-        }
-        if(s == "size") cout << v.size() << "\n";
-        if(s == "pop") {
-            if(v.size() != 0) {
-                cout << v.back() << "\n";
-                v.pop_back();
-            } else {
-                cout << -1 <<"\n";
-            }
-        }
-        if(s == "empty") {
-            if(v.size()) {
-                cout << 0 << "\n";
-            } else cout << 1 << "\n";
-        }
+        v.push_back(s);
     }
+    
+    for(int i{}; i < n; i++) {
+        for (int j{}; j < v.at(i).length(); j++) {
+            if(v.at(i).at(j) == '(')
+                stack.push_back(v.at(i).at(j));
+            else if(v.at(i).at(j) == ')' && stack.size() != 0 && stack.back() == '(')
+                stack.pop_back();
+            else if(v.at(i).at(j) == ')' && (stack.size() == 0 || stack.back() != '('))
+                stack.push_back(v.at(i).at(j));
+        }
+        if(stack.size()) cout << "NO" << "\n";
+        else if(!stack.size()) cout << "YES" << "\n";
+        
+        stack.clear();
+    }
+    
     
     return 0;
 }

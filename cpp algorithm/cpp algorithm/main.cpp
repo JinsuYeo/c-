07b84d8b252f0b3a604b39dda5a40961a73859cd
JinsuYeo <68772263+JinsuYeo@users.noreply.github.com>
2022-01-28 10:12:53 +0900

    정렬 연습 백준 11656번
    erase() 메서드로 문자열의 첫번쨰 문자 하나씩 지우면서 벡터에 삽입한 뒤, sort 함수로 정렬 후 출력.

--- cpp algorithm/cpp algorithm/main.cpp
+++ cpp algorithm/cpp algorithm/main.cpp
@@ -5,55 +5,31 @@
 
 using namespace std;
     
+bool compare(string a, string b) {
+    return a < b;
+}
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
     
-    int n{};
-    vector<int> v;
     string s;
+    vector<string> v;
     
-    cin >> n;
-    for(int i{}; i < n; i++) {
-        cin >> s;
-        if(s == "push_front") {
-            int t;
-            cin >> t;
-            v.insert(v.begin(), t);
+    cin >> s;
+    string t = s;
+    for(int i{}; i < s.length(); i++) {
+        v.push_back(t);
+        if(s.size()){
+            t.erase(t.begin());
         }
-        if(s == "push_back") {
-            int t;
-            cin >> t;
-            v.push_back(t);
-        }
-        if(s == "pop_front") {
-            if(v.size()) {
-                cout << v.front() << "\n";
-                v.erase(v.begin());
-            } else cout << -1 << "\n";
-        }
-        if(s == "pop_back") {
-            if(v.size()) {
-                cout << v.back() << "\n";
-                v.pop_back();
-            } else cout << -1 << "\n";
-        }
-        if(s == "size") {
-            cout << v.size() << "\n";
-        }
-        if(s == "empty") {
-            if(!v.size()) cout << 1 << "\n";
-            else cout << 0 << "\n";
-        }
-        if(s == "front") {
-            if(!v.size()) cout << -1 << "\n";
-            else cout << v.front() << "\n";
-        }
-        if(s == "back") {
-            if(!v.size()) cout << -1 << "\n";
-            else cout << v.back() << "\n";
-        }
+    }
+    
+    sort(v.begin(), v.end(), compare);
+    
+    for (int i{}; i < v.size(); i++) {
+        cout << v.at(i) << "\n";
     }
     
     return 0;

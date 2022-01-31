65379fca9219a3e5adf39bd38aa67634c6b9882b
JinsuYeo <68772263+JinsuYeo@users.noreply.github.com>
2022-01-31 15:26:59 +0900

    백준 11653번 소인수분해
    2~n까지의 수로 n을 나눴을 떄 나머지가 0인 제수를 출력하고 피제수 n을 제수로 나눠준 값으로 바꾼 뒤 다시 제수를 2부터~nㅈ까지 반복.

--- cpp algorithm/cpp algorithm/main.cpp
+++ cpp algorithm/cpp algorithm/main.cpp
@@ -2,41 +2,22 @@
 
 using namespace std;
 
-int arr[1000001];
-
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
   
-    int n{}, t{1000000};
+    int n{};
+    cin >> n;
     
-    arr[0] = 1;
-    arr[1] = 1;
-    
-    for(int i{2}; i <= t; i++) {
-        for (int j{i*2}; j <= t; j += i) {
-            if(arr[j] == 0) arr[j] = 1;
+    for(int i{2}; i <= n; i++) {
+        if(n%i == 0){
+            cout << i << "\n";
+            n /= i;
+            i = 1;
         }
     }
     
-    while (cin >> n) {
-        if(n == 0) return 0;
-        
-        int a{}, b{};
-        for(int i{2}; i <= n; i++) {
-            if(arr[i] == 0 && arr[n-i] == 0) {
-                a = i;
-                b = n-i;
-                break;
-            }
-        }
-        
-        if(!a) cout << "Goldbach's conjecture is wrong.\n";
-        else {
-            cout << n << " = " << a << " + " << b << "\n";
-        }
-    }
     
     return 0;
 }

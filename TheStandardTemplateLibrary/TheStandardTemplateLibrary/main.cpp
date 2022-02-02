ccf032723c517fda29454de1cf1c45cb9fb6a2bb
JinsuYeo <68772263+JinsuYeo@users.noreply.github.com>
2022-02-03 00:39:07 +0900

    STL Algorithm 사용 연습
    find(), count(), count_if(), replace(), all_of(), string_transform()...
    
    count_if()에는 람다식 들어감. 조건에 일치할 경우 count됨. 조건은 predicate라고 함(true or false 반환하는 것을 의미함).
    
    string_transform()은 string 말고 컨테이너에도 사용 가능함.
    
    ::toupper에서 ::는 global namespace를 사용함을 의미함. (std::touuper()이 아닌 cctype 헤더파일의 global namespace의 toupper()를 사용할 것임을 컴파일러에게 알려줌)

--- TheStandardTemplateLibrary/TheStandardTemplateLibrary/main.cpp
+++ TheStandardTemplateLibrary/TheStandardTemplateLibrary/main.cpp
@@ -1,119 +1,130 @@
+// Section 20
+// Algorithms
 #include <iostream>
 #include <vector>
-#include <set>
-#include <map>
 #include <list>
-#include <string>
+#include <algorithm>
+#include <cctype>
 
-void display(const std::vector<int> &vec)  {
-    std::cout << "[ ";
-    for(auto const &i: vec) std::cout << i << " ";
-    std::cout << "]" << std::endl;
-}
-
-void test1() {
-    std::cout << "\n=========================" << std::endl;
-    std::vector<int> nums1 {1, 2, 3, 4, 5};
-    auto it = nums1.begin(); // point to 1
-    std::cout << *it << std::endl;
-    
-    it++;                   // point to 2
-    std::cout << *it << std::endl;
-    
-    it += 2;                // point to 4
-    std::cout << *it << std::endl;
-
-    it -= 2;                // point to 2
-    std::cout << *it << std::endl;
-
-    it = nums1.end() - 1;    // point to 5
-    std::cout << *it << std::endl;
-}
-
-void test2() {
-    std::cout << "\n=========================" << std::endl;
-    
-    std::vector<int> nums1 {1, 2, 3, 4, 5};
-    
-    std::vector<int>::iterator it = nums1.begin();
-    while (it != nums1.end()) {
-        std::cout << *it << std::endl;
-        it++;
+class Person {
+    std::string name;
+    int age;
+public:
+    Person() = default;
+    Person(std::string name, int age)
+        : name{name}, age{age}  {}
+    bool operator<(const Person &rhs) const {
+        return this->age < rhs.age;
     }
-    
-    it = nums1.begin();
-    while (it != nums1.end()) {
-        *it = 0;
-        it++;
+    bool operator==(const Person &rhs) const {
+        return (this->name == rhs.name && this->age == rhs.age);
     }
-    
-    display(nums1);
-}
+};
 
-void test3() {
-    std::cout << "\n=========================" << std::endl;
-    
-    std::vector<int> nums1 {1, 2, 3, 4, 5};
-    
-    std::vector<int>::const_iterator it1 = nums1.begin();
-    // auto it1 = nums1.cbegin();
-    
-    while (it1 != nums1.end()) {
-        std::cout << *it1 << std::endl;
-        it1++;
-    }
-    
-    it1 = nums1.begin();
-    while (it1 != nums1.end()) {
-//        *it1 = 0; // Compiler error - read only
-        it1++;
-    }
-    
-    display(nums1);
-}
+// Find an element in a container
+void find_test() {
+    std::cout << "\n========================" << std::endl;
 
-void test4() {
-    std::vector<int> vec {1, 2, 3, 4};
-    auto it1 = vec.rbegin();
-    while (it1 != vec.rend()) {
-        std::cout << *it1 << std::endl;
-        it1++;
-    }
+    std::vector<int> vec {1,2,3,4,5};
     
-    std::list<std::string> name {"Larry", "Moe", "Curly"};
-    auto it2 = name.crbegin();
-    std::cout << *it2 << std::endl;
-    it2++;
-    std::cout << *it2 << std::endl;
+    auto loc = std::find(std::begin(vec), std::end(vec), 1);
     
-    std::map<std::string, std::string> favorite {
-        {"Frank", "C++"},
-        {"Bill", "Java"},
-        {"James", "Haskell"}
+    if (loc != std::end(vec))
+        std::cout << "Found the number: " << *loc <<  std::endl;
+    else
+        std::cout << "Couldn't find the number" << std::endl;
+        
+    std::list<Person> players {
+        {"Larry", 18},
+        {"Moe", 20},
+        {"Curly", 21}
     };
-    auto it3 = favorite.begin();
-    while (it3 != favorite.end()) {
-        std::cout << it3->first << ": " << it3->second << std::endl;
-        it3++;
-    }
+    
+    auto loc1 = std::find(players.begin(), players.end(), Person{"Moe", 20});
+    if (loc1 != players.end())
+        std::cout << "Found  Moe" << std::endl;
+    else
+        std::cout << "Moe not found" << std::endl;
 }
 
-void test5() {
-    std::vector<int> vec {1, 2, 3, 4, 5 ,6 ,7, 8, 9, 10};
-    auto start = vec.begin() + 2;
-    auto finish = vec.end() - 3;
-    
-    while(start != finish) {
-        std::cout << *start << std::endl;
-        start++;
-    }
+// Count the number of elements in a container
+void count_test() {
+    std::cout << "\n========================" << std::endl;
+
+     std::vector<int> vec {1,2,3,4,5,1,2,1};
+     
+     long num = std::count(vec.begin(), vec.end(), 1);
+     std::cout << num << " occurrences found" << std::endl;
 }
-int main(){
-    test1();
-    test2();
-    test3();
-    test4();
-    test5();
+
+// Count the number of occurences of an element in a container
+// based on a predicate using a lambda expression
+
+void count_if_test() {
+    std::cout << "\n========================" << std::endl;
+
+    // count only if the element is even
+    std::vector<int> vec {1,2,3,4,5,1,2,1,100};
+    long num = std::count_if(vec.begin(), vec.end(), [](int x) { return x %2 == 0; });
+    std::cout << num << " even numbers found" << std::endl;
     
+    num = std::count_if(vec.begin(), vec.end(), [](int x) { return x %2 != 0; });
+    std::cout << num << " odd numbers found" << std::endl;
+    
+    // how can we determine how many elements in vec are >= 5?
+    num = std::count_if(vec.begin(), vec.end(), [](int x) { return x>=5;   });
+    std::cout << num << "  numbers are >= 5" << std::endl;
+
+}
+
+// Replace occurrences of elements in a container
+void replace_test() {
+    std::cout << "\n========================" << std::endl;
+
+     std::vector<int> vec {1,2,3,4,5,1,2,1};
+     for (auto i: vec) {
+         std::cout << i << " ";
+     }
+     std::cout << std::endl;
+     
+    std::replace(vec.begin(), vec.end(), 1, 100);
+    for (auto i: vec) {
+         std::cout << i << " ";
+     }
+    std::cout << std::endl;
+}
+
+void all_of_test() {
+    std::vector<int> vec1 {1,3,5,7,9,1,3,13,19,5};
+    if (std::all_of(vec1.begin(), vec1.end(), [](int x) { return x > 10; }))
+        std::cout << "All the elements are > 10" << std::endl;
+    else
+        std::cout << "Not all the elements are > 10" << std::endl;
+    
+    if (std::all_of(vec1.begin(), vec1.end(), [](int x) { return x < 20; }))
+        std::cout << "All the elements are < 20" << std::endl;
+    else
+        std::cout << "Not all the elements are < 20" << std::endl;
+}
+
+// Transform elements in a container - string in this example
+void string_transform_test() {
+    std::cout << "\n========================" << std::endl;
+
+    std::string str1 {"This is a test"};
+    std::cout << "Before transform: " << str1 << std::endl;
+    std::transform(str1.begin(), str1.end(), str1.begin(), ::toupper);
+    std::cout << "After transform: " << str1 << std::endl;
+}
+
+int main() {
+    find_test();
+    count_test();
+    count_if_test();
+    replace_test();
+    all_of_test();
+    string_transform_test();
+
     return 0;
 }
+

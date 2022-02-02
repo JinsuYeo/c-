124823d74e955ba1b46338837251e822c6aed101
JinsuYeo <68772263+JinsuYeo@users.noreply.github.com>
2022-02-02 14:44:28 +0900

    그래프 연습 백준 11724번 연결 요소의 개수
    그래프 구현해준 뒤, 1~n번 노드 순서로 반복문 돌려서 시작 노드가 이미 방문되었는지 체크하고 . 방문 안되었다면 연결된 노드들 모두 방문 처리 해주고 count 세줌. 이미 시작 노드가 방문 되었다면 count 세지 않음. -> 방문 안 했었다면 새로운 연결 요소이고 이미 시작 노드가 방문 처리 되었다면 다른 노드에서 이미 세준 연결 요소이기 때문.
    count를 출력.
    
    그래프를 직접 그린 후 생각하니 나름 쉽게 풀 수 있었음.
    풀고 다른 사람 풀이보니 생각 못했는데 나는 dfs로 푼것임. -> dfs나 bfs로 그래프 순회한 뒤 방문한 곳 체크하고, 안된 곳은 끊어진 곳이니 bfs를 한 횟수가 연결 요소의 개수임.

--- cpp algorithm/cpp algorithm/main.cpp
+++ cpp algorithm/cpp algorithm/main.cpp
@@ -1,44 +1,21 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
-#include <queue>
 
 using namespace std;
 
-int dc[1001];
-int bc[1001];
-int v;
-int matrix[1001][1001];
-
+int visited[1001];
+int v, e, c;
 vector<int> arr[1001];
 
-void dfs(int x) {
-    if(dc[x]) return;
-    dc[x] = true;
-    cout << x << " ";
-    for(int i{1}; i <= v; i++) {
-        if(matrix[x][i] && !dc[i]) {
-            dfs(i);
-        }
+int connected(int node){
+    if(visited[node]) return true;
+    visited[node] = true;
+    for(int i{}; i < arr[node].size(); i++) {
+        int x = arr[node][i];
+        connected(x);
     }
-}
-
-void bfs(int start) {
-    queue<int> q;
-    q.push(start);
-    bc[start] = true;
-    
-    while (!q.empty()) {
-        int x = q.front();
-        q.pop();
-        cout << x << " ";
-        for (int i{1}; i <= v; i++) {
-            if(matrix[x][i] && !bc[i]) {
-                bc[i] = true;
-                q.push(i);
-            }
-        }
-    }
+    return false;
 }
 
 int main() {
@@ -46,19 +23,20 @@ int main() {
     cin.tie(NULL);
     cout.tie(NULL);
     
-    int s{}, e{};
-    
-    cin >> v >> e >> s;
+    cin >> v >> e;
     for(int i{}; i < e; i++) {
         int x, y;
         cin >> x >> y;
-        matrix[x][y] = 1;
-        matrix[y][x] = 1;
+        arr[x].push_back(y);
+        arr[y].push_back(x);
     }
     
-    dfs(s);
-    cout << "\n";
-    bfs(s);
+    for(int i{1}; i <= v; i++) {
+        int check = connected(i);
+        if(!check) c++;
+    }
+    
+    cout << c << "\n";
     
     return 0;
 }

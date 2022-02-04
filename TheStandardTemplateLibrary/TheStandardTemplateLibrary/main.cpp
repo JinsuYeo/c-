15b9363d68d2c37e5a691f5d9b5d12f9673c8e78
JinsuYeo <68772263+JinsuYeo@users.noreply.github.com>
2022-02-05 00:27:10 +0900

    List
    list와 forward_list는 모두 시퀀스 컨테인이며, 요소를 비연속 메모리에 저장함.
    .at() or []로 직접 요소에 액세스가 불가능.
    but 요소 찾았을 때 삽입, 삭제가 매우 효율적임.
    
    리스트는 이중 연결 리스트로 작동함.
    직접 요소 접근 불가능하나 양방향 순회 가능.
    모든 곳에서 요소 삽입, 삭제가 효율적(상수 시간).
    iterator invalidate 발생 가능.
    
    요소는 메모리에서 연속적이지 않으며, 앞, 뒤에대한 참조를 가짐. -> 이러한 링크를 관리하는데 약간의 오버헤드 있음.
    
    요소 삽입, 삭제 위해 요소에 대한 iterator있어야 하며 보통 find() 메서드로 수행됨(선형 시간).
    -> 삽입, 삭제를 많이 해야하고 직접 요소에 접근할 필요 없을 때, 컨테이너로 리스트 고르는 것은 좋은 선택임.
    
    insert() 사용 시 iterator 앞에 두번째 인자를 삽입하며, iterator 무효화되지 않고 여전히 참조하던 곳 가리킴.
    erase() 사용 시 iterator 무효화됨. 필요한 경우 재설정하거나 반환값 받아 사용.
    
    forward_list는 c++11에 STL에 추가됨.
    정방향 리스트는 단일 연결 리스트로 작동.
    -> list보다 오버헤드 적지만 한방향으로만 순회 가능.
    push_front(), pop_front()만 가능.
    size()사용 불가 -> size위해 list 내부에서 count 유지해야 해서 객체 크기가 증가하여 비효율적이기 때문.
    insert_after(), erase_after() 사용해야 함.
    
    참고) resize() 할 경우 원래 크기보다 크면 남은 공간 기본값으로 초기화해줌(int형일 경우 0, 객체인 경우 default 생성자 사용해 기본값 생성함 -> 생성자 오버로딩 시, default 생성자 없으면 오류).
    std::advance()는 사용하기 위해 #include <iterator> 필요. iterator 가리키는 곳 바꿔줌.

--- TheStandardTemplateLibrary/TheStandardTemplateLibrary/main.cpp
+++ TheStandardTemplateLibrary/TheStandardTemplateLibrary/main.cpp
@@ -1,117 +1,164 @@
 #include <iostream>
-#include <deque>
-#include <vector>
+#include <list>
 #include <algorithm>
+#include <iterator> // for std::advance
 
-// template function to display any deque
+class Person {
+    friend std::ostream &operator<<(std::ostream &os, const Person &p);
+    std::string name;
+    int age;
+public:
+    Person() : name{"Unknown"}, age{0} {}
+    Person(std::string name, int age)
+        : name{name}, age{age}  {}
+    bool operator<(const Person &rhs) const {
+        return this->age < rhs.age;
+    }
+    bool operator==(const Person &rhs) const {
+        return (this->name == rhs.name && this->age == rhs.age);
+    }
+};
+
+std::ostream &operator<<(std::ostream &os, const Person &p) {
+    os << p.name << ":" << p.age;
+    return os;
+}
+
+
 template <typename T>
-void display(const std::deque<T> &d) {
-    std::cout << "[";
-    for (const auto &elem: d)
+void display(const std::list<T> &l) {
+    std::cout << "[ ";
+    for (const auto &elem: l) {
         std::cout << elem << " ";
-    std::cout <<  "]"<< std::endl;
+    }
+    std::cout << "]" << std::endl;
 }
 
 void test1() {
     std::cout << "\nTest1 =========================" << std::endl;
 
-    std::deque<int> d {1,2,3,4,5};
-    display(d);
+    std::list<int> l1 {1,2,3,4,5};
+    display(l1);
     
-    d = {2,4,5,6};
-    display(d);
+    std::list<std::string> l2;
+    l2.push_back("Back");
+    l2.push_front("Front");
+    display(l2);
     
-    std::deque<int> d1 (10, 100);    // ten 100s in the deque
-    display(d1);
+    std::list<int> l3;
+    l3 = {1,2,3,4,5,6,7,8,9,10};
+    display(l3);
     
-    d[0]  = 100;
-    d.at(1) = 200;
-    display(d);
+    std::list<int> l4 (10, 100);
+    display(l4);
 }
 
 void test2() {
-    // push and pops
     std::cout << "\nTest2 =========================" << std::endl;
 
-    std::deque<int> d {0,0,0};
-    display(d);
+    std::list<int> l {1,2,3,4,5,6,7,8,9,10};
+    display(l);
+    std::cout << "Size: " << l.size() << std::endl;
     
-    d.push_back(10);
-    d.push_back(20);
-    display(d);
+    std::cout << "Front : " << l.front() << std::endl;
+    std::cout << "Back  : " << l.back() << std::endl;
     
-    d.push_front(100);
-    d.push_front(200);
-    display(d);
-    
-    std::cout << "Front: " << d.front() << std::endl;
-    std::cout << "Back : " << d.back() << std::endl;
-    std::cout << "Size  : " << d.size() << std::endl;
-    
-    d.pop_back();
-    d.pop_front();
-    display(d);
+    l.clear();
+    display(l);
+    std::cout << "Size: " << l.size() << std::endl;
 }
 
 void test3() {
-    // insert all even numbers into the back of a deque and all
-    // odd numbers into the front
     std::cout << "\nTest3 =========================" << std::endl;
-    
-    std::vector<int> vec {1,2,3,4,5,6,7,8,9,10};
-    std::deque<int> d;
-    
-    for (const auto &elem: vec) {
-        if (elem % 2 == 0)
-            d.push_back(elem);
-        else
-            d.push_front(elem);
-    }
-    display(d);
-}
 
+    std::list<int> l {1,2,3,4,5,6,7,8,9,10};
+    display(l);
+    
+    l.resize(5);
+    display(l);
+    
+    l.resize(10);
+    display(l);
+    
+    std::list<Person> persons;
+    persons.resize(5);             // uses the Person default constructor
+    display(persons);
+    
+}
 void test4() {
-    // push front vs. back ordering
     std::cout << "\nTest4 =========================" << std::endl;
 
-    std::vector<int> vec {1,2,3,4,5,6,7,8,9,10};
-    std::deque<int> d;
-    
-    for (const auto &elem: vec) {
-        d.push_front(elem);
+    std::list<int> l {1,2,3,4,5,6,7,8,9,10};
+    display(l);
+    auto it = std::find(l.begin(), l.end(), 5);
+    if (it != l.end()) {
+        l.insert(it, 100);
     }
-    display(d);
+    display(l);
     
-    d.clear();
+    std::list<int> l2 {1000,2000,3000};
+    l.insert(it, l2.begin(), l2.end());
+    display(l);
     
-     for (const auto &elem: vec) {
-        d.push_back(elem);
-    }
-    display(d);
+    std::advance(it, -4);       // point to the 100
+    std::cout << *it << std::endl;
+    
+    l.erase(it);                    // remove the 100 - iterator becomes invalid
+    display(l);
+    
 }
 
 void test5() {
-    // Same as test4 using std::copy
     std::cout << "\nTest5 =========================" << std::endl;
 
-    std::vector<int> vec {1,2,3,4,5,6,7,8,9,10};
-    std::deque<int> d;
+    std::list<Person>  stooges {
+        {"Larry", 18},
+        {"Moe", 25},
+        {"Curly", 17}
+    };
     
-    std::copy(vec.begin(), vec.end(), std::front_inserter(d));
-    display(d);
+    display(stooges);
+    std::string name;
+    int age{};
+    std::cout << "\nEnter the name of the next stooge: ";
+    getline(std::cin, name);
+    std::cout << "Enter their age: ";
+    std::cin >> age;
     
-    d.clear();
+    stooges.emplace_back(name, age);
+    display(stooges);
     
-    std::copy(vec.begin(), vec.end(), std::back_inserter(d));
-    display(d);
+    // Insert Frank before Moe
+    auto it = std::find(stooges.begin(), stooges.end(), Person{"Moe", 25});
+    if (it != stooges.end())
+        stooges.emplace(it, "Frank", 18);
+    display(stooges);
 }
 
+void test6() {
+    std::cout << "\nTest6 =========================" << std::endl;
+    
+    std::list<Person>  stooges {
+        {"Larry", 18},
+        {"Moe", 25},
+        {"Curly", 17}
+    };
+    
+    display(stooges);
+    stooges.sort();
+    display(stooges);
+}
+
+
 int main() {
+    
     test1();
     test2();
     test3();
     test4();
     test5();
+    test6();
+    std::cout << std::endl;
     return 0;
 }
 

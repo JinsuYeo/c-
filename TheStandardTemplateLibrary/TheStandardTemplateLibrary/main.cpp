c689ae1da35487b61fbbf53f15312bafa354c9fb
JinsuYeo <68772263+JinsuYeo@users.noreply.github.com>
2022-02-05 21:23:03 +0900

    Map
    map은 사전과 비슷함. 단워와 뜻처럼 키와 값의 쌍으로 저장됨(std::pair 이용해서).
    키로 정렬되며 키는 중복될 수 없음.
    key 이용해 요소에 직접 접근 가능.
    
    front, back의 개념 없음.
    [], at()이용해 요소 삽입, 수정 가능. -> 문법 매우 쉽게 만들어줌.

--- TheStandardTemplateLibrary/TheStandardTemplateLibrary/main.cpp
+++ TheStandardTemplateLibrary/TheStandardTemplateLibrary/main.cpp
@@ -1,119 +1,95 @@
 #include <iostream>
+#include <map>
 #include <set>
 
-class Person {
-    friend std::ostream &operator<<(std::ostream &os, const Person &p);
-    std::string name;
-    int age;
-public:
-    Person() : name{"Unknown"}, age{0} {}
-    Person(std::string name, int age)
-        : name{name}, age{age}  {}
-    bool operator<(const Person &rhs) const {
-        return this->age < rhs.age;
-    }
-    bool operator==(const Person &rhs) const {
-        return (this->name == rhs.name && this->age == rhs.age);
-    }
-};
 
-std::ostream &operator<<(std::ostream &os, const Person &p) {
-    os << p.name << ":" << p.age;
-    return os;
+void display(const std::map<std::string, std::set<int>> &m) {
+    std::cout << "[ ";
+    for (const auto &elem: m) {
+        std::cout << elem.first << ": [ " ;
+            for (const auto &set_elem : elem.second)
+                 std::cout << set_elem <<  " ";
+            std::cout << "] " ;
+    }
+    std::cout << "]  " << std::endl;
 }
 
-template <typename T>
-void display(const std::set<T> &l) {
+template <typename T1, typename T2>
+void display(const std::map<T1, T2> &l) {
     std::cout << "[ ";
     for (const auto &elem: l) {
-        std::cout << elem << " ";
+        std::cout << elem.first << ":" << elem.second << " ";
     }
-    std::cout << "]" << std::endl;
+    std::cout << "]  " << std::endl;
 }
 
 void test1() {
-    // Sets
+    // Maps
     std::cout << "\nTest1 =========================" << std::endl;
-    std::set<int> s1 {1, 4, 3, 5, 2};
-    display(s1);
+    std::map<std::string, int> m {
+        {"Larry", 3},
+        {"Moe", 1},
+        {"Curly", 2}
+    };
+    display(m);
     
-    s1 = {1,2,3,1,1,2,2,3,3,4,5};
-    display(s1);
+    m.insert( std::pair<std::string, int>("Anna", 10));
+    display(m);
     
-    s1.insert(0);
-    s1.insert(10);
+    m.insert(std::make_pair("Joe", 5));
+    display(m);
     
-    display(s1);
+    m["Frank"] = 18;
+    display(m);
     
-    if (s1.count(10))
-        std::cout << "10 is in the set" << std::endl;
-    else
-        std::cout << "10 is NOT in the set" << std::endl;
-
-    auto it = s1.find(5);
-    if (it != s1.end())
-        std::cout << "Found: " << *it << std::endl;
+    m["Frank"] += 10;
+    display(m);
+    
+    m.erase("Frank");
+    display(m);
+    
+    std::cout << "Count for Joe: " << m.count("Joe") << std::endl;
+    std::cout << "Count for Frank: " << m.count("Frank") << std::endl;
+    
+    auto it = m.find("Larry");
+    if (it != m.end())
+        std::cout << "Found: " << it->first << " : " << it->second << std::endl;
         
-    s1.clear();
+    m.clear();
     
-    display(s1);
+    display(m);
+
 }
 
 void test2() {
     std::cout << "\nTest2 =========================" << std::endl;
-    std::set<Person> stooges {
-        {"Larry", 1},
-        {"Moe", 2},
-        {"Curly", 3}
+    
+    std::map<std::string, std::set<int>> grades  {
+        {"Larry", {100, 90} },
+        {"Moe",  {94} },
+        {"Curly", {80, 90, 100} }
     };
-    display(stooges);           // Note the order of display!  operator<
-
-    stooges.emplace("James", 50);
-    display(stooges);
-
-    stooges.emplace("Frank", 50); // NO -- 50 already exists
-    display(stooges);
     
-    auto it = stooges.find(Person{"Moe", 2});
-    if (it != stooges.end())
-        stooges.erase(it);
+    display(grades);
     
-    display(stooges);
+    grades["Larry"].insert(95);                 // insert 95 into the set of grades for "Larry"
     
-    it = stooges.find(Person("XXXX", 50));      // Will remove James!!!!
-                                                                     // uses operator<
-    if (it != stooges.end())
-        stooges.erase(it);
-    display(stooges);
+    display(grades);
+    
+    auto it = grades.find("Moe");
+    if (it != grades.end())                         // found "Moe"
+    {
+            it->second.insert(1000);            // insert 1000 into Moe's set of grades
+    }                                                         // it->first is the key, it->second is the value (set)
+    display(grades);
 }
 
-void test3() {
-    std::cout << "\nTest3 =========================" << std::endl;
-    
-    std::set<std::string> s {"A", "B", "C"};
-    display(s);
-    
-    auto result = s.insert("D");
-    display(s);
-    
-    std::cout << std::boolalpha;
-    std::cout << "first: " <<  *(result.first)<< std::endl;
-    std::cout  << "second: " << result.second << std::endl;
-    
-    std::cout << std::endl;
 
-    result = s.insert("A");
-    display(s);
-    
-    std::cout << std::boolalpha;
-    std::cout << "first: " <<  *(result.first)<< std::endl;
-    std::cout  << "second: " << result.second << std::endl;
-}
+
 
 int main() {
     test1();
     test2();
-    test3();
     return 0;
 }
 

c27550181feed1b17430597b8ede124a3cf494d7
JinsuYeo <68772263+JinsuYeo@users.noreply.github.com>
2022-02-08 01:05:54 +0900

    백준 1654번 이진 탐색 이용해 푸는 방법
    다른 사람 코드 보고 배움. k개의 랜선 값 입력 받은 뒤 랜선의 최대 길이 max로 잡고 최소 길이 1로 잡은 뒤 이진 탐색 이용해서 k개의 랜선을 (랜선 길이 / mid)한 값의 합(몫의 합)이 n이 되는 최댓값 구함.
    
    코드 안봤으면 절대 생각 못했을 듯. 내가 푼 것도 나름 알고리즘 생각해서 답 맞추긴 했는데 확실히 더 정답에 가까운 풀이, 코드는 잇는 듯.
    아쉬운 점은 자료형 잘못 설정해서 틀린 것과 예외 생각 못했던 것(2^n-1로 나누어 떨어지는 수) 정도. 비슷한 문제 나온다면 이진 탐색 활용해서 풀 수 있다는 것 기억할 것.

--- cpp algorithm/cpp algorithm/main.cpp
+++ cpp algorithm/cpp algorithm/main.cpp
@@ -1,58 +1,39 @@
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <cmath>
-#include <array>
-
+#include<iostream>
 using namespace std;
 
-int k, n;
-array<long long, 10001> arr;
+int K, N, ans;
+int line[10001];
+int  maxi = 0;
+long long mid, high, low;
 
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+int main()
+{
+    cin >> K >> N;
+    
+    for (int i = 0; i < K; i++)
+    {
+        cin >> line[i];
+        if (maxi < line[i]) maxi = line[i];
+    }
+    low = 1;
+    high = maxi;
+    ans = 0;
 
-    cin >> k >> n;
-    for (int i{1}; i <= k; i++) {
-        cin >> arr[i];
+    while (low <= high)
+    {
+        mid = (low + high) / 2;
+        int cnt = 0;
+        for (int i = 0; i < K; i++)
+            cnt += line[i] / mid;
+
+        if (cnt >= N) {
+            low = mid + 1;
+            if (ans < mid) ans = mid;
+        }
+        else {// (cnt < N)
+            high = mid - 1;
+        }
     }
     
-    long long max{}, sum{}, result{};
-    for (int i{1}; i < 32; i++) {
-        for(int j{1}; j <= k; j++) {
-            sum += (arr[j]/(pow(2, i)-1));
-        }
-        if(sum >= n && max < pow(2, i) - 1) max = pow(2, i) - 1;
-        if(sum < n) break;
-        sum = 0;
-    }
-    
-    max = result;
-    
-    while (1) {
-        bool flag = false;
-        for (int i{1}; i < 32; i++) {
-            for(int j{1}; j <= k; j++) {
-                sum += (arr[j]/(max + (pow(2, i)-1)));
-            }
-            if(sum >= n) result = max + pow(2, i) - 1;
-            if(i == 1 && sum < n) {
-                flag = true;
-                break;
-            }
-            if(sum < n) break;
-            sum = 0;
-        }
-        if (flag) {
-            break;
-        }
-        sum = 0;
-        max = result;
-    }
-    
-       
-    cout << result << "\n";
-    
-    return 0;
+    cout << ans;
 }

2480e7858b101bd88e3a16cba46a318f5b0e6f7b
JinsuYeo <68772263+JinsuYeo@users.noreply.github.com>
2022-02-08 18:53:16 +0900

    이분 탐색 연습 백준 10815번 숫자 카드
    첫번 쨰로 입력 받은 것 정렬해서 이분 탐색으로 두번쨰 입력 중 첫번 쨰 입력에서 입력 받았는 지 확인해서 출력.
    그냥 std::find()로 되나 해봤는데 시간초과 -> O(N)이라고 함.
    set::find()는 O(logN)이라길래 set에 첫번째 입력 저장한 후 find() 해봤더니 정답처리 됨.

--- cpp algorithm/cpp algorithm/main.cpp
+++ cpp algorithm/cpp algorithm/main.cpp
@@ -1,45 +1,37 @@
 #include <iostream>
 #include <algorithm>
+#include <vector>
+#include <set>
 
 using namespace std;
 
-int n, c;
-long long house[200001];
-long long mid, high, low, result;
+set<int> have;
+vector<int> check;
 
-int check(long long d) {
-    int count{1};
-    long long cur = house[1];
-    for (int i{2}; i <= n; i++) {
-        if (house[i] - cur >= d) {
-            count++;
-            cur = house[i];
-        }
-    }
-    return count;
-}
+int n, m;
+long long mid, high, low, result;
 
 int main()
 {
-    cin >> n >> c;
-    
-    for (int i{1}; i <= n; i++)
-        cin >> house[i];
-    
-    sort(house, house + n + 1);
-    
-    low = 1;
-    high = house[n] - house[1];
-
-    while (low <= high) {
-        mid = (low + high) / 2;
-        if(check(mid) >= c) {
-            result = max(result, mid);
-            low = mid + 1;
-        } else high = mid - 1;
+    cin >> n;
+    for (int i{1}; i <= n; i++) {
+        int t{};
+        cin >> t;
+        have.insert(t);
+    }
+    cin >> m;
+    for (int i{1}; i <= m; i++) {
+        int t{};
+        cin >> t;
+        check.push_back(t);
     }
     
-    cout << result << "\n";
-    
+    for(int i{}; i < check.size(); i++) {
+        auto it = have.find(check.at(i));
+        if (it != have.end()) {
+            cout << 1 << " ";
+        } else cout << 0 << " ";
+    }
+        
     return 0;
 }

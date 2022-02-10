470d1abace6651d6c12994b776c3ef48c179c465
JinsuYeo <68772263+JinsuYeo@users.noreply.github.com>
2022-02-10 21:56:13 +0900

    분할 정복 연습 백준1517번 버블 소트
    쉬울 줄 알았는데 정답률 너무 낮아서 입력값 범위 보니 n <= 500000이라 O(n^2)인 버블 정렬 못함.
    +버블 소트 시 swap이 일어나는 횟수 세야해서 sort() 이용 불가.
    -> 트리 이용하거나 병합 정렬 사용해야 함.
    
    병합 정렬 구현한 뒤, mid+1 ~ end 구간이 start ~ mid 구간보다 먼저 삽입될 때, count++ 하면 될거라고 생각하고 시작.
    
    병합 정렬이 정확히 기억이 안나서 다시 공부함.
    + 생각해보니 count++하면 3, 4 와 1, 2 있을 때,  count는 2가 되지만 사실 4번 일어남.
    
    결국 다른 사람 코드 보고 풀었는데, count++ 가 아니라 count += mid-i+1; 로 구현해야 했음(2, 4  1, 3 에서 1이 먼저 삽입될 때, count += 1-0+1(2), 3이 4보다 먼저 삽입될 때,  i = 1이므로 1-1+1로 count += 1이됨. 2 4 1 3 -> 2 1 4 3 -> 1 2 4 3 -> 1 2 3 4로 3번 맞음)
    
    병합 정렬 구현한 것에서 저 부분만 바꿨는데 안되길래 한참 고민했는데, arr를 sorted로 바꿔주는 작업을 안해서 그런 것이었음. 병합 정렬 구현이 아직 많이 서투른 듯. 병합 정렬 좀 더 익숙해지기.

--- cpp algorithm/cpp algorithm/main.cpp
+++ cpp algorithm/cpp algorithm/main.cpp
@@ -1,59 +1,60 @@
 #include <iostream>
-#include <cstdio>
 #include <array>
-#include <string>
+#include <vector>
 
 using namespace std;
 
-int n;
-array<array<char, 3072*2>, 3072> arr;
+long long n, c;
 
-void draw(int x, int y, int size) {
-    if (size == 3) {
-        for (int i{x-1}; i <= x + 1; i++) {
-            for (int j{y-2}; j <= y + 2; j++) {
-                if(i == x-1 && j == y) {
-                    arr[i][j] = '*';
-                }
-                if (i == x && j == y - 1) {
-                    arr[i][j] = '*';
-                }
-                if (i == x && j == y + 1) {
-                    arr[i][j] = '*';
-                }
-                if (i == x + 1 && j >= y - 2 && j <= y + 2) {
-                    arr[i][j] = '*';
-                }
-            }
+array<long long, 500001> arr;
+array<long long, 500001> sorted;
+
+void merge(int start, int mid, int end) {
+    int i = start;
+    int j = mid + 1;
+    int k = start;
+    while (i <= mid && j <= end) {
+        if (arr[i] <= arr[j]) {
+            sorted[k++] = arr[i++];
+        } else {
+            c += mid-i+1;
+            sorted[k++] = arr[j++];
         }
-        return;
     }
-    int t = size/2;
-    draw(x - t, y, t);
-    draw(x, y - t, t);
-    draw(x, y + t, t);
+    while (i <= mid) {
+        sorted[k++] = arr[i++];
+    }
+    while (j <= end) {
+        sorted[k++] = arr[j++];
+    }
+    
+    for (int i{start}; i <= end; i++) {
+        arr[i] = sorted[i];
+    }
 }
 
+void merge_sort(int start, int end) {
+    if (start < end) {
+        int mid = (start + end) / 2;
+        merge_sort(start, mid);
+        merge_sort(mid+1, end);
+        merge(start, mid, end);
+    }
+}
 
-int main()
-{
+int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     
     cin >> n;
-    
-    for (int i{}; i < n; i++) {
-        arr[i].fill(' ');
+    for (int i{1}; i <= n; i++) {
+        cin >> arr[i];
     }
     
-    draw(n-2, n-1, n);
+    merge_sort(1, n);
     
-    for (int i{}; i < n; i++) {
-        for (int j{}; j < 2*n-1; j++) {
-            cout << arr[i][j];
-        }
-        cout << "\n";
-    }
+    cout << c;
     
     return 0;
 }
+

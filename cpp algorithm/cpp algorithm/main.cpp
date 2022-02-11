90a7ff4977d8b8586ffb5743ebc5a514cbd0ece8
JinsuYeo <68772263+JinsuYeo@users.noreply.github.com>
2022-02-11 13:06:08 +0900

    그리디 연습 백준 11047번 동전 0
    그리디 알고리즘으로 구현.
    입력값들 priority_queue(=q)에 넣은 뒤(가장 큰 동전부터 사용하기 위해 우선순위 큐 사용), k(가치의 합) / q,top() 을 count에 더해주고(동전 사용 개수), k%=q.top()으로 동전으로 만들고 남은 값을 k에 할당해줌. q.pop()으로 사용한 동전 제거. q가 빌 때까지 반복.

--- cpp algorithm/cpp algorithm/main.cpp
+++ cpp algorithm/cpp algorithm/main.cpp
@@ -1,96 +1,30 @@
 #include <iostream>
-#include <vector>
-#include <set>
+#include <queue>
 #include <algorithm>
 
 using namespace std;
 
-#define X first
-#define Y second
+int n, m, c;
+priority_queue<int> q;
 
-const int MAX = 1 << 30;
-
-typedef pair<int, int> pairii;
-vector<pairii> v;
-
-int dist(int low, int high){
-    int lowx = v[low].X;
-    int lowy = v[low].Y;
-    int highx = v[high].X;
-    int highy = v[high].Y;
+int main(void) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
     
-    int distx = highx - lowx;
-    int disty = highy - lowy;
-    
-    return distx*distx + disty*disty;
-}
-
-int check(int low, int high) {
-    if (low == high) return MAX;
-    if (low + 1 >= high) return dist(low, high);
-    
-    int min = dist(low, high);
-    int mid = (low+high)/2;
-    int t{};
-    
-    if ((t = check(low, mid)) < min) {
-        min = t;
-    }
-    if ((t = check(mid+1, high)) < min) {
-        min = t;
-    }
-    
-    vector<pairii> v2;
-    int line = v[mid].X;
-    for (int i{mid}; i >= low; i--) {
-        int x = v[i].X;
-        int dis = line - x;
-        dis *= dis;
-        if (min <= dis) break;
-        else v2.push_back({v[i].Y, v[i].X});
-    }
-    for (int i{mid+1}; i <= high; i++) {
-        int x = v[i].X;
-        int dis = line - x;
-        dis *= dis;
-        if (min <= dis) break;
-        else v2.push_back({v[i].Y, v[i].X});
-    }
-    
-    int size = static_cast<int>(v2.size());
-    if (!size) return min;
-    
-    sort(v2.begin(), v2.end());
-    
-    for(int i{}; i < size; i++){
-        int ix = v2[i].Y, iy = v2[i].X;
-        for(int j{i+1}; j < size; j++){
-            int jx = v2[j].Y, jy = v2[j].X;
-            int distx = jx - ix, disty = jy - iy;
-            if(min <= disty * disty) break;
-            if(min <= distx * distx) continue;
-            
-            int dist = distx*distx + disty*disty;
-            if(dist < min)
-                min = dist;
-        }
-    }
-    
-    return min;
-}
-
-int main() {
-    int n;
-    cin >> n;
+    cin >> n >> m;
     for (int i{}; i < n; i++) {
-        int x, y;
-        cin >> x >> y;
-        v.push_back({x, y});
+        int t{};
+        cin >> t;
+        q.push(t);
     }
     
-    sort(v.begin(), v.end());
+    while (!q.empty()) {
+        c += m/q.top();
+        m %= q.top();
+        q.pop();
+    }
     
-    cout << check(0, n-1);
+    cout << c;
     
     return 0;
 }

d37bc7c6b0b2dec25a72aa31f803658e2ce2a0e7
JinsuYeo <68772263+JinsuYeo@users.noreply.github.com>
2022-02-13 01:36:22 +0900

    그리디 연습 백준 1744번 수 묶기
    0보다 작은수와 큰수를 나누어서 벡터에 삽입한 뒤(음수, 양수 나누어줌), 음수는 오름차 순으로 정렬, 양수는 내림차 순으로 정렬해주고, 1이 들어오면 그대로 result에 더해줌(1은 곱하면 없어지기 때문에 곱하지 않고 더해주어야 합이 더 커짐). 그 후, 0이 입력되었는지 확인 후, 입력되었고 음수의 개수가 홀수라면 음수 중 가장 큰 값(벡터의 마지막에 있는 요소)를 없애줌(n*0 = 0, 음수는 없애주어야 합이 가장 크게 됨). 음수가 짝수라면 벡터의 처음부터 2개씩 곱한 뒤 result에 더해줌. 양수의 경우 홀수일 땐 양수 벡터의 가장 마지막 수(양수 중 가장 작은 수)는 result에 더해주고 양수가 짝수일 때 벡터의 처음부터 2개 씩 곱해준 뒤 result에 더해줌(내림차 순 정렬되어 있음).
    
    정답률이 낮은 그리디 문제 중 처음으로 혼자 맞춘 문제인 듯. 하다보니 좀 감이 잡힘. 거의 있을 수 있는 경우의 수들을 모두 체크해서 각각의 경우에 따라 처리해주는 느낌.

--- cpp algorithm/cpp algorithm/main.cpp
+++ cpp algorithm/cpp algorithm/main.cpp
@@ -1,106 +1,58 @@
 #include <iostream>
+#include <vector>
 #include <algorithm>
-#include <array>
-#include <string>
 
 using namespace std;
-int arr[1000][1000];
-int R, C;
-string result;
 
-int main(void) {
+int n, result;
+
+vector<int> p;
+vector<int> z;
+vector<int> m;
+
+int main(void){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-        
-    cin >> R >> C;
-    for (int i{}; i < R; i++) {
-        for (int j{}; j < C; j++) {
-            cin >> arr[i][j];
-        }
+    
+    cin >> n;
+    for (int i{}; i < n; i++) {
+        int t{};
+        cin >> t;
+        if (t < 0) {
+            m.push_back(t);
+        } else if(t == 0) z.push_back(t);
+        else if(t == 1) result += 1;
+        else p.push_back(t);
     }
     
-    if(R%2) {
-        for (int i{}; i < R; i++) {
-            for (int j{}; j < C-1; j++) {
-                if (i % 2 == 0) {
-                    result += 'R';
-                } else {
-                    result += 'L';
-                }
-            }
-            if (i != R - 1) {
-                result += 'D';
-            }
+    sort(m.begin(), m.end());
+    sort(p.begin(), p.end(), greater<int>());
+    
+    if (m.size() % 2 == 0) {
+        for (int i{}; i < m.size(); i += 2) {
+            result += m.at(i) * m.at(i+1);
         }
-    } else if(!(R%2) && C%2) {
-        for (int j{}; j < C; j++) {
-            for (int i{}; i < R-1; i++) {
-                if (j % 2 == 0) {
-                    result += 'D';
-                } else {
-                    result += 'U';
-                }
-            }
-            if (j != C-1) {
-                result += 'R';
-            }
-        }
-    } else if(!(R%2) && !(C%2)) {
-        int min_v{987654321};
-        int y{}, x{};
-        for (int i{}; i < R; i++) {
-            for (int j{}; j < C; j++) {
-                if ((i + j) % 2 && min_v > arr[i][j]) {
-                    min_v = arr[i][j];
-                    y = i;
-                    x = j;
-                }
-            }
-        }
-        int nR = y%2 ? y-1 : y;
-        for (int i{}; i < nR; i++) {
-            for (int j{}; j < C - 1; j++) {
-                if (i%2 == 0) {
-                    result += 'R';
-                } else {
-                    result += 'L';
-                }
-            }
-            result += 'D';
-        }
-        int nC = x;
-        for (int i{}; i < nC; i++) {
-            if (i % 2 == 0) {
-                result += "DR";
-            } else {
-                result += "UR";
-            }
-        }
-        for (int i{nC}; i < C - 1; i++) {
-            if (i % 2== 0) {
-                result += "RD";
-            } else {
-                result += "RU";
-            }
-        }
-        if (y % 2 == 0) {
-            nR = R - (y+2);
+    } else if(m.size()%2 != 0) {
+        if (!z.empty()) {
+            m.pop_back();
         } else {
-            nR = R - (y+1);
+            result += m.back();
+            m.pop_back();
         }
-        for (int i{}; i < nR; i++) {
-            result += 'D';
-            for (int j{}; j < C - 1; j++) {
-                if (i % 2 == 0) {
-                    result += 'L';
-                } else {
-                    result += 'R';
-                }
-            }
+        for (int i{}; i < m.size(); i+=2) {
+            result += m.at(i) * m.at(i+1);
         }
     }
     
-    cout << result << "\n";
+    if (p.size()%2 != 0) {
+        result += p.back();
+        p.pop_back();
+    }
+    for (int i{}; i < p.size(); i+=2) {
+        result += p.at(i) * p.at(i+1);
+    }
+    
+    cout << result << '\n';
     
     return 0;
 }

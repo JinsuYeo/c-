803832d52977aa9e62e9f591ecb268c71ca04ad7
JinsuYeo <68772263+JinsuYeo@users.noreply.github.com>
2022-02-13 23:20:55 +0900

    함수 내부에서 throw exception
    함수 내부에서 예외 throw 했을 때, C++은 catch 핸들러를 찾으려고 시도, catch 핸들러는 함수 내부에 없을 수 있고, 아예 존재하지 않을 수 있음(이 때는 프로그램이 비정상 종료됨).
    
    만약 예외 핸들러가 존재한다면, 함수(throw가 있는)를 호출한 콜 스택의 함수 중 하나여야 함.
    그리고 C++은 그것을 찾기 위해 stack을 unwind 할 것임.
    
    참고) https://banaba.tistory.com/42
    
    - 스택 되감기의 발생
    
    C++ 예외처리에서 예외를 던지는 throw는 try구문 안에 있어야 하며, throw 발생 시 예외를 받아주는 catch구문으로 점프하게 됩니다.
    함수 내에서 try, catch 구문이 없고 throw만 발생시키는 함수도 있는데, 이 경우 catch를 찾기 위해 함수 외부를 찾게 됩니다.
    예외를 던진 함수의 외부에서 catch를 찾기 위해 현재의 스택정보를 정리하고 빠져나가는 스택 되감기(Stack Unwinding)가 발생합니다.
    
     - 발생 이유
    
    우선, 스택 되감기가 발생하는 이유를 알기 위해선 스택 프레임에 대해 알고 있어야 합니다.
    함수가 호출될 때 스택에는 매개변수로 전달되는 인자 값, 함수의 반환 값, 함수를 마치고 복귀할 번지수, 지역변수 등의 정보가 저장되는데, 이러한 정보를 스택프레임 이라고 합니다.
    임시적인 정보를 저장하기 위해 스택을 사용하되, 푸시 회수와 팝 회수가 일치하므로 함수를 호출하고 나면 호출 전의 상태와 같습니다.
    이처럼 함수는 호출자(caller)와 피호출자(callee)가 서로 협조하여 스택의 항상성을 유지합니다.
    본론으로 돌아와서, 스택 되감기의 이유는 스택의 항상성 유지에 있습니다.
    함수 내에서 예외가 발생하였을 때 외부의 catch구문으로 바로 점프한다면 스택 프레임의 항상성을 유지할 수 없게 됩니다.
    때문에 예외 발생시, 함수 내부에 catch 구문이 없다면 스택프레임의 항상성 유지를 위해 호출원을 거슬러 올라가여 스택을 정리하며 빠져나가게 됩니다.

--- ExceptionHandling/ExceptionHandling/main.cpp
+++ ExceptionHandling/ExceptionHandling/main.cpp
@@ -2,6 +2,12 @@
 
 using namespace std;
 
+double calculate_mpg(int miles, int gallons) {
+    if (gallons == 0)
+        throw 0;
+    return static_cast<double>(miles) / gallons;
+}
+
 int main(int argc, const char * argv[]) {
     int miles{};
     int gallons{};
@@ -12,17 +18,8 @@ int main(int argc, const char * argv[]) {
     cout << "Enter the gallons: ";
     cin >> gallons;
     
-//    if (gallons != 0) {
-//        miles_per_gallon = static_cast<double>(miles) / gallons;
-//        cout << "Result: " << miles_per_gallon << "\n";
-//    } else {
-//        cerr << "Sorry. can't divide by zero" << endl;
-//    }
-    
     try {
-        if (gallons == 0)
-            throw 0;
-        miles_per_gallon = static_cast<double>(miles) / gallons;
+        miles_per_gallon = calculate_mpg(miles, gallons);
         cout << "Result: " << miles_per_gallon << "\n";
     } catch (int &ex) {
         cerr << "Sorry. you can't divide by zero" << endl;

ca637cee2e7f5cafd8415f1c8913976265c80733
JinsuYeo <68772263+JinsuYeo@users.noreply.github.com>
2022-02-15 00:36:21 +0900

    Class-level exception
    클래스 메서드, 생성자, 소멸자에서 예외 throw 가능.
    
    C++에서 소멸자는 no except by default, 즉, 기본적으로는 예외를 throw 하지 않음.
    만약 다른 곳에서 예외가 throw된 결과 소멸자가 호출된 경우, 그 후 소멸자가 다시 예외를 throw하게 되면 원래 있던 예외의 catch 블록에 도달할 수 없기 때문에 문제가 발생함.
    소멸자가 예외를 던질 수 있는 유일한 경우는 자체적으로 예외를 처리하는 경우이며, 매우 가능성이 낮은 사용 사례임.
    가장 좋은 방법은 소멸자에서 예외 던지지 않는 것.
    
    클래스 메서드의 경우, 예외 처리는 일반 함수에서 본것처럼 작동함.
    
    생성자의 경우, 생성자는 값을 반환하지 않기 때문에 boolean 값이나 error code 반환이 불가능. + 생성자가 실패할 수 있음(예외 발생 가능. ex) 동적 할당 실패, 객체 초기화할 수 없을 때). -> 생성자는 예외 던질 수 있음.
    예를 들어 예시 코드처럼 음수값으로 계좌 객체 초기화를 불가능하게 하려면 0보다 작은 인수 들어오면 throw로 예외 던짐. -> catch로 받아서 처리 가능. 생성자가 반환값이 없어도 예외 이용해 처리 가능.
    
    참고) 클래스 생성자의 초기화 리스트에 try 블록 넣을 수 있음.

--- ExceptionHandling/ExceptionHandling/main.cpp
+++ ExceptionHandling/ExceptionHandling/main.cpp
@@ -1,38 +1,24 @@
 #include <iostream>
+#include <memory>
+#include "Account.h"
+#include "Checking_Account.h"
+#include "Savings_Account.h"
+#include "Trust_Account.h"
+#include "Account_Util.h"
+#include "IllegalBalanceException.h"
 
-class DivideByZeroException {
-};
-class NegativeValueException {
-};
-
-double calculate_mpg(int miles, int gallons) {
-    if (gallons == 0)
-        throw DivideByZeroException();
-    else if (miles < 0 || gallons < 0)
-        throw NegativeValueException();
-    return static_cast<double>(miles) / gallons;
-}
+using namespace std;
 
 int main() {
-    int miles {};
-    int gallons {};
-    double miles_per_gallon {};
-    
-    std::cout << "Enter the miles: ";
-    std::cin >> miles;
-    std::cout << "Enter the gallons: ";
-    std::cin >> gallons;
-    
     try {
-        miles_per_gallon = calculate_mpg(miles, gallons);
-        std::cout << "Result: " << miles_per_gallon << std::endl;
-    } catch (const DivideByZeroException &ex) {
-        std::cerr << "Sorry, you can't divide by zero" << std::endl;
-    } catch (const NegativeValueException &ex) {
-        std::cerr << "Sorry, one of your parameters is negative" << std::endl;
+        std::unique_ptr<Account> moes_account = std::make_unique<Checking_Account>("Moe", -10.0);
+        std::cout << *moes_account << std::endl;
     }
-    std::cout << "Bye" << std::endl;
-
+    catch (const IllegalBalanceException &ex)
+    {
+        std::cerr << "Couldn't create account - negative balance" << std::endl;
+    }
+    std::cout << "Program completed successfully" << std::endl;
     return 0;
 }
 

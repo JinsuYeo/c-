605b2a5927247bc3e762a549ee162b4502ab28c4
JinsuYeo <68772263+JinsuYeo@users.noreply.github.com>
2022-02-20 01:59:12 +0900

    완전 탐색 연습 백준 1987번 알파벳
    세로 R칸(y), 가로 C칸(x).
    각 칸에 알파벳 대문자 들어가고 좌측 상단(1, 1)에서 시작해서 상하좌우로 이동 가능. 이미 지나온 알파벳 있는 칸에는 갈 수 없음.
    이 때 최대로 갈 수 있는 칸 출력. 첫 칸도 포함.
    문자열과 map으로 구현했던 문제와 비슷.
    string으로 입력받고 x = index%C, y = index/C.
    백트래킹(dfs 종료 후 전단계로 돌아가서 배열 등을 원상복귀해야 함).
    -> 시간 초과. l-value 참조로 인자 받아 사용해봄. 그래도 시간 초과.
    뭔가 에러가 있는 것 같아서 디버깅해봤는데 딱히 문제는 없었음.
    반례 찾다가 답글에 어떤 사람이 set을 이용해 visited 처리해서 시간초과 낫다는 것 보고, map 대신 원시 배열 사용하여 사용된 알파벳을 체크하도록 바꿈. -> 정답 처리됨.
    map이 구현이랑 보기 편하다는 장점 있지만, 원시 배열보다 시간 복잡도 느린듯. 아마 특히 erase().
    옛날에 썼던 문제는 erase() 사용할 필요가 없었음. 그래도 시도해봐서 경험은 쌓인 듯. + 비슷한 문제 풀어본 경험 잊지 않고 사용한 것은 좋았음.

--- cpp algorithm/cpp algorithm/main.cpp
+++ cpp algorithm/cpp algorithm/main.cpp
@@ -1,47 +1,36 @@
 #include <iostream>
 #include <algorithm>
 #include <vector>
+#include <string>
+#include <map>
 
 using namespace std;
 
-int arr[9][9];
-bool row[9][10];
-bool col[9][10];
-bool small[9][10];
+int R, C;
+string s;
+bool visited[27];
+int result;
 
-int change(int x, int y){
-    return (y/3)*3 + x/3;
-}
+typedef struct {
+    int y;
+    int x;
+} DIR;
+DIR dir[4] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 
-void dfs(int c){
-    int y{c/9};
-    int x{c%9};
+void dfs(int &&idx, int &&cnt){
+    int y{idx/C};
+    int x{idx%C};
     
-    if (c == 81) {
-        for (int i{}; i < 9; i++) {
-            for (int j{}; j < 9; j++) {
-                cout << arr[i][j] << " ";
-            }
-            cout << '\n';
-        }
-        exit(0);
-    }
+    if (cnt > result) result = cnt;
     
-    if (arr[y][x]) {
-        dfs(c+1);
-    } else {
-        for (int k{1}; k <= 9; k++) {
-            if(!row[y][k] && !col[x][k] && !small[change(x, y)][k]){
-                arr[y][x] = k;
-                row[y][k] = true;
-                col[x][k] = true;
-                small[change(x, y)][k] = true;
-                dfs(c+1);
-                arr[y][x] = 0;
-                row[y][k] = false;
-                col[x][k] = false;
-                small[change(x, y)][k] = false;
-            }
+    for (int i{}; i < 4; i++) {
+        int nexty = y + dir[i].y;
+        int nextx = x + dir[i].x;
+        if (nextx < 0 || nextx >= C || nexty < 0 || nexty >= R) continue;
+        if (!visited[s[nexty*C + nextx]-'A']) {
+            visited[s[nexty*C + nextx]-'A'] = true;
+            dfs(nexty*C + nextx, cnt+1);
+            visited[s[nexty*C + nextx]-'A'] = false;
         }
     }
 }
@@ -50,18 +39,17 @@ int main(void) {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     
-    for (int i{}; i < 9; i++) {
-        for (int j{}; j < 9; j++) {
-            cin >> arr[i][j];
-            if (arr[i][j]) {
-                row[i][arr[i][j]] = true;
-                col[j][arr[i][j]] = true;
-                small[change(j, i)][arr[i][j]] = true;
-            }
-        }
+    cin >> R >> C;
+    for (int i{}; i < R; i++) {
+        string t;
+        cin >> t;
+        s += t;
     }
     
-    dfs(0);
+    visited[s[0]-'A'] = true;
+    dfs(0, 1);
+    
+    cout << result << '\n';
 
     return 0;
 }

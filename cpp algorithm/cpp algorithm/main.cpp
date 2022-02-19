0bab023b38294043ebf1dfd43f5c43f1567cf8b1
JinsuYeo <68772263+JinsuYeo@users.noreply.github.com>
2022-02-19 11:25:47 +0900

    완전 탐색 연습 백준 5014번 스타트링크
    F(1~1000000)층으로 이루어진 고층 건물.
    G층에 가야하고 현재 S층에 있음.
    위로 U층을 가는 버튼, 아래로 D층을 가는 버튼 두개만 있을 때, G층을 가기 위해 버튼 눌러야 하는 최소 횟수 출력. 만약 갈 수 없다면 use the stairs 출력. 0 <= U, D <= 1000000.
    
    ex)
    10, 1, 10, 2, 1(F, S, G, U, D)일 때,
    1 -> 3 -> 5-> 7 -> 9 -> 8 -> 10
    6 출력해야함.
    
    BFS로 횟수 세며 탐색. -> pair로 큐에 현재 위치와 횟수 저장.
    S에서 시작해서 S+U, S-D가 1~F사이에 있다면 S+U, S-D를 탐색. visited도 처리해줌.
    G층에 도착하면 bfs는 횟수 리턴. 도착 못하면 -1 리턴.
    리턴 결과에 따라 출력.

--- cpp algorithm/cpp algorithm/main.cpp
+++ cpp algorithm/cpp algorithm/main.cpp
@@ -1,91 +1,53 @@
 #include <iostream>
 #include <algorithm>
 #include <string>
-#include <cstring>
+#include <queue>
 #include <vector>
 
 using namespace std;
 
-const int MAX {2002};
+const int MAX {1000001};
 
-int N;
-typedef struct{
-    int x1;
-    int x2;
-    int y1;
-    int y2;
-} RECT;
+int F, S, G, U, D;
 
-typedef struct{
-    int x;
-    int y;
-} DIR;
-DIR dir[4] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+queue<pair<int, int>> q;
+bool visited[MAX];
 
-vector<RECT> rects;
-bool arr[MAX][MAX]; // 직사각형 그릴 배열(-500 ~ 500)
-bool visited[MAX][MAX];
-
-int C;
-
-void dfs(int x, int y){
-    visited[y][x] = true;
+int bfs(int start){
+    q.push(make_pair(start, 0));
+    visited[start] = true;
     
-    for (int i{}; i < 4; i++) {
-        int nexty = y + dir[i].y;
-        int nextx = x + dir[i].x;
+    while (!q.empty()) {
+        int cur = q.front().first;
+        int count = q.front().second;
+        q.pop();
         
-        if (nexty < 0 || nexty >= MAX || nextx < 0 || nextx >= MAX) {
-            continue;
+        if (cur == G) {
+            return count;
         }
-        if (!visited[nexty][nextx] && arr[nexty][nextx]) {
-            dfs(nextx, nexty);
-            visited[nexty][nextx] = true;
+        
+        if (cur + U <= F && !visited[cur+U]) {
+            q.push(make_pair(cur+U, count+1));
+            visited[cur+U] = true;
+        }
+        if (cur - D >= 1 && !visited[cur-D]) {
+            q.push(make_pair(cur-D, count+1));
+            visited[cur-D] = true;
         }
     }
+    
+    return -1;
 }
 
 int main(void) {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     
-    cin >> N;
-    for (int i{}; i < N; i++) {
-        int x1, y1, x2, y2;
-        cin >> x1 >> y1 >> x2 >> y2;
-        RECT rect = {(x1 + 500)*2, (x2 + 500)*2, (y1 + 500)*2, (y2 + 500)*2};
-        rects.push_back(rect);
-    }
+    cin >> F >> S >> G >> U >> D;
     
-    for (int i{}; i < rects.size(); i++) {
-        RECT rect = rects[i];
-        memset(*(arr + rect.y1) + rect.x1, 1, rect.x2-rect.x1 + 1);
-        memset(*(arr + rect.y2) + rect.x1, 1, rect.x2-rect.x1 + 1);
-        for (int j{rect.y1}; j <= rect.y2; j++) {
-            arr[j][rect.x1] = 1;
-        }
-        for (int j{rect.y1}; j <= rect.y2; j++) {
-            arr[j][rect.x2] = 1;
-        }
-    }
-    bool zero{false};
-    for(int i{}; i < MAX; i++) {
-        for (int j{}; j < MAX; j++) {
-            if (arr[i][j] && !visited[i][j]) {
-                C++;
-                dfs(j, i);
-            }
-        }
-    }
-    if (arr[1000][1000]) {
-        zero = true;
-    }
-    
-    if (zero) {
-        C--;
-    }
-    
-    cout << C;
+    int result = bfs(S);
+    if (result != -1) cout << result << '\n';
+    else cout << "use the stairs\n";
     
     return 0;
 }

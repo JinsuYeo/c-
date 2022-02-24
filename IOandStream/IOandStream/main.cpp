113edae7f5e5913fe06ca4712f3f8d43d994a3d1
JinsuYeo <68772263+JinsuYeo@users.noreply.github.com>
2022-02-25 05:32:02 +0900

    Writing to a Text file
    기본적으로 C++는 쓸 파일이름과 동일한 파일 없으면 생성하고 이미 존재하면 내용 잘리거나 제거됨.
    파일을 close하는 것은 출력 파일에 특히 중요한데 아직 파일에 기록되지 않은 buffer를 flush하기 때문.
    기본적으로 입력 떄와 비슷하고, 파일을 쓰기 전용으로 열 때 manipulator로 std::ios::trunc(default, 파일 이미 존재하면 원래 내용 지워짐), std::ios::app(파일의 EOF에 append), std::ios::ate(존재하는 파일 열 때는 처음 위치를 기존 파일의 끝으로 설정하나, random access 이용해 자유롭게 이동 가능함)가 있음.

--- IOandStream/IOandStream/main.cpp
+++ IOandStream/IOandStream/main.cpp
@@ -4,33 +4,18 @@
 #include <string>
 
 int main(void) {
-    std::fstream in_file;
+    std::ofstream out_file {"./output.txt", std::ios::app};
+    
+    if (!out_file) {
+        std::cerr << "Error creating file" << std::endl;
+        exit(1);
+    }
     std::string line;
-    char c;
-    in_file.open("./text.txt");
-    if (!in_file) {
-        std::cerr << "Problem opening file" << std::endl;
-        std::exit(1);
-    }
-        
-    while(in_file.get(c)) {
-        std::cout << c;
-    }
+    std::cout << "Enter something to write to the file: ";
+    std::getline(std::cin, line);
+    out_file << line << std::endl;
     
-    in_file.close();
-    
-    in_file.open("./text.txt");
-    if (!in_file) {
-        std::cerr << "Problem opening file" << std::endl;
-        std::exit(1);
-    }
-    
-    while(std::getline(in_file, line)) {
-        std::cout << std::setw(10) << std::left << line
-                  << std::endl;
-    }
-    
-    in_file.close();
+    out_file.close();
     
     return 0;
 }

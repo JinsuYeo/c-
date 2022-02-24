795e06e80544e1a081c225f044a57f7267a98896
JinsuYeo <68772263+JinsuYeo@users.noreply.github.com>
2022-02-25 03:06:50 +0900

    파일에서 읽기 실습
    참고) Window의 경우 파일 경로에 \ 들어갈 때 \\ 사용해야 함. or / 써도 됨 -> behind the scene에서 적절한 것으로(\) 바꿔줌.
    Window가 아닌 다른 운영체제에선 파일 경로에 / 사용돼서 상관 없음.
    
    참고2) Xcode는 product의 경로가 main.cpp가 있는 폴더가 아님. 따라서 Product -> Scheme -> Edit scheme -> Run -> Working directory를 main.cpp가 있는(or 사용할파일이 있는) 위치로 바꿔줘야 현재 위치를 open()의 인자로 넣어줘서 파일 읽어올 수 있음.

--- IOandStream/IOandStream/main.cpp
+++ IOandStream/IOandStream/main.cpp
@@ -1,22 +1,25 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 
-int main() {
-    std::ifstream in_file;
+int main(void) {
+    std::fstream in_file;
     std::string line;
     int num;
     double total;
     
-    in_file.open("../test.txt");
+    in_file.open("./text.txt");
     if (!in_file) {
         std::cerr << "Problem opening file" << std::endl;
-        return 1;
+        std::exit(1);
     }
+    
     in_file >> line >> num >> total;
     std::cout << line << std::endl;
     std::cout << num << std::endl;
     std::cout << total << std::endl;
     in_file.close();
+    
     return 0;
 }
 

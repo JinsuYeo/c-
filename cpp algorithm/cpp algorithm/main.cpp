61a7b6c5c87259873f5d716134dd0e5c4f2e524f
JinsuYeo <68772263+JinsuYeo@users.noreply.github.com>
2022-02-26 03:02:02 +0900

    구현 연습 백준 9093번 단어 뒤집기
    어제 배운 stringstream + reverse()로 구현해봄.
    원래 스택으로 구현하는 문제인듯.

--- cpp algorithm/cpp algorithm/main.cpp
+++ cpp algorithm/cpp algorithm/main.cpp
@@ -1,69 +1,30 @@
 #include <iostream>
-#include <vector>
+#include <string>
+#include <sstream>
 #include <algorithm>
 
 using namespace std;
 
-long long T;
-int N, M;
-long long result;
+int N;
+string s;
 
 int main(void) {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     
-    cin >> T;
     cin >> N;
-    vector<int> A(N);
+    cin.ignore(1);
     for (int i{}; i < N; i++) {
-        cin >> A[i];
-    }
-    
-    cin >> M;
-    vector<int> B(M);
-    for (int i{}; i < M; i++) {
-        cin >> B[i];
-    }
-    
-    vector<long long> v1, v2;
-    
-    int low{}, high{};
-    long long sum{};
-    while (low < A.size()) {
-        sum += A[high++];
-        v1.push_back(sum);
-        if (high == A.size()) {
-            low ++;
-            high = low;
-            sum = 0;
+        string t;
+        getline(cin, s);
+        stringstream ss{s};
+        while(ss >> t){
+            reverse(t.begin(), t.end());
+            cout << t << " ";
         }
+            
+        cout << endl;
     }
-    
-    low = 0;
-    high = 0;
-    sum = 0;
-    while (low < B.size()) {
-        sum += B[high++];
-        v2.push_back(sum);
-        if (high == B.size()) {
-            low ++;
-            high = low;
-            sum = 0;
-        }
-    }
-    
-    sort(v1.begin(), v1.end());
-    sort(v2.begin(), v2.end());
-    
-    for (int i{}; i < v1.size(); i++) {
-        long long num = T - v1[i];
-        long long low = lower_bound(v2.begin(), v2.end(), num) - v2.begin();
-        long long high = upper_bound(v2.begin(), v2.end(), num) - v2.begin();
-        
-        if (num == v2[low]) result += high - low;
-    }
-    
-    cout << result << "\n";
     
     return 0;
 }

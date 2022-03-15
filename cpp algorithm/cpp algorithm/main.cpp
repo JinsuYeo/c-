17b33fb8a2676bc92c2c79f267cb428d042534e8
JinsuYeo <68772263+JinsuYeo@users.noreply.github.com>
2022-03-15 15:15:22 +0900

    DP 연습 백준 15988번 1, 2, 3 더하기 3
    적으면서 규칙 찾아보니
    DP[i] = DP[i-3] + DP[i-2] + DP[i-1]; 이었음. 처음에 이렇게 제출했다가 틀려서 문제 확인하니 MOD  안해주어서 틀렸었음.
    -> MOD 고쳐주니 정답처리됨.

--- cpp algorithm/cpp algorithm/main.cpp
+++ cpp algorithm/cpp algorithm/main.cpp
@@ -4,51 +4,27 @@
 
 using namespace std;
 
-const int MAX = 1001;
+const int MAX = 1000001;
+const int MOD = 1000000009;
 
-int T, M, idx;
-vector<int> V;
-vector<pair<int, vector<int>>> dp(MAX);
+int T, N;
+int dp[MAX];
 
 int main(void) {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     
-    cin >> T;
-    vector<int> arr(T+1);
-    for (int i{1}; i <= T; i++)
-        cin >> arr[i];
-    
-    
-    dp[1] = {1,{arr[1]}};
-    M = 1;
-    V = {arr[1]};
-    for (int i{1}; i < T+1; i++) {
-        int result{};
-        vector<int> temp;
-        for (int j{1}; j < i; j++) {
-            if (arr[i] > arr[j]) {
-                result = max(result, dp[j].first + 1);
-                if(result == dp[j].first + 1) {
-                    temp = dp[j].second;
-                    temp.push_back(arr[i]);
-                }
-            }
-        }
-        if (!result) {
-            dp[i] = {1,{arr[i]}};
-        } else {
-            dp[i] = {result, temp};
-        }
-        if(result >= M) {
-            M = result;
-            V = temp;
-        }
+    dp[1] = 1;
+    dp[2] = 2;
+    dp[3] = 4;
+    for (int i{4}; i < MAX; i++) {
+        dp[i] = (((dp[i-3] + dp[i-2]) % MOD) + dp[i-1]) % MOD;
     }
     
-    cout << M << '\n';
-    for (auto e: V) {
-        cout << e << " ";
+    cin >> T;
+    for (int i{}; i < T; i++) {
+        cin >> N;
+        cout << dp[N] << '\n';
     }
     
     return 0;

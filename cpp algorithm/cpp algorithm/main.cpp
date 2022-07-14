fb96c88b3843a4e8d9c874d4de82c4333cf8f957
JinsuYeo <68772263+JinsuYeo@users.noreply.github.com>
2022-07-14 23:38:59 +0900

    백준 1920번 수찾기
    unordered_map(map과 달리 정렬되지 않는 대신 효율 증가)과 vector 이용해 풀음.
    
    N과 M이 100000이하라 1초 내에 돌아가게 하기 위해 map 사용, 처음엔 A, B 모두 map 사용했다가 자동 정렬되어서 출력 순서 안맞음.
    하나 벡터로 수정하기 전에 unordered_map 사용해 보았는데 순서가 삽입순서가 아니었음.

--- cpp algorithm/cpp algorithm/main.cpp
+++ cpp algorithm/cpp algorithm/main.cpp
@@ -1,36 +1,36 @@
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 #include <algorithm>
 
 using namespace std;
 
-const int MAX = 100001;
-const int MOD = 9901;
-
-int T, result;
-int arr[MAX][3];
-int dp[MAX][3];
+int N, M;
+unordered_map<int, int> A;
+vector<int> B;
 
 int main(void) {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     
-    cin >> T;
-    
-    for (int i{}; i < 3; i++)
-        dp[1][i] = 1;
-    
-    for (int i{2}; i <= T; i++) {
-        dp[i][0] = (((dp[i-1][1] + dp[i-1][2]) % MOD) + dp[i-1][0]) % MOD;
-        dp[i][1] = (dp[i-1][0] + dp[i-1][2]) % MOD;
-        dp[i][2] = (dp[i-1][0] + dp[i-1][1]) % MOD;
+    cin >> N;
+    for (int i{}; i < N; i++) {
+        int t{};
+        cin >> t;
+        A.insert(make_pair(t, t));
     }
     
-    for (int i{}; i < 3; i++) {
-        result = (((dp[T][1] + dp[T][2]) % MOD) + dp[T][0]) % MOD;
+    cin >> M;
+    for (int i{}; i < M; i++) {
+        int t{};
+        cin >> t;
+        B.push_back(t);
     }
     
-    cout << result << '\n';
+    for (auto b: B) {
+        if(A[b]) cout << 1 << " ";
+        else cout << 0 << " ";
+    }
     
     return 0;
 }
